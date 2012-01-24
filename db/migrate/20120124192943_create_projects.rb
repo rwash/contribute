b037b1470f56643851f3718495dabfc67d752435
rockwe34 <rockwe34@socialmedia.cas.msu.edu>
2012-01-24 16:42:51 -0500

    Adding migration for creating the projects table

--- db/migrate/20120124192943_create_projects.rb
+++ db/migrate/20120124192943_create_projects.rb
@@ -1,8 +1,18 @@
 class CreateProjects < ActiveRecord::Migration
-  def change
+  def up
     create_table :projects do |t|
+			t.string :name
+			t.string :shortDescription
+			t.text	:longDescription
+			t.decimal :fundingGoal
+			t.date :startDate
+			t.date :endDate
 
       t.timestamps
     end
   end
+
+	def down
+		drop_table :projects
+	end
 end
