eb22623364ef4feabe8feaec5d86f6c402f693b6
Mitchell Thelen <mthelen@socialmedia.cas.msu.edu>
2012-01-27 15:34:40 -0500

    fixed date bug, added date validation - testing + thumbnails soon

--- app/models/project.rb
+++ app/models/project.rb
@@ -17,11 +17,24 @@ class Project < ActiveRecord::Base
 	validates :fundingGoal, :presence => true, :format => { :with => /^\d+??(?:\.\d{0,2})?$/ }, :numericality => {:greater_than_or_equal_to => Project.MIN_FUNDING_GOAL, :less_than_or_equal_to => Project.MAX_FUNDING_GOAL}
 	validates :created_at, :presence => true
 	validates :active, :presence => true
-	#todo: dates
+
+	validates :startDate, :presence => true
+	validates :endDate, :presence => true
+	validate :valid_start_end_date
 
 	def initialize(attributes = nil, options = {})
 		super
 		self.active = true
 		self.created_at = Date.today
+	end
+
+	def valid_start_end_date
+		if !startDate || !endDate
+			return
+		elsif startDate < Date.today || startDate > Date.today + 1.months
+			errors.add(:startDate, "must be on or within a month from today")
+		elsif endDate < startDate + 1.days || endDate > startDate + 1.month
+			errors.add(:endDate, "must be greater or equal to the day after the start day and less or equal to a month within the start date")
+		end
 	end
 end
