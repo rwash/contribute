92e66475ef4e3f8c4ccdf8589cb412863d3aa2bd
Mitchell Thelen <mthelen@socialmedia.cas.msu.edu>
2012-01-27 15:56:09 -0500

    finished testing for start/end date

--- test/unit/project_test.rb
+++ test/unit/project_test.rb
@@ -7,8 +7,8 @@ class ProjectTest < ActiveSupport::TestCase
 		assert project.errors[:name].any?
 		assert project.errors[:shortDescription].any?
 		assert project.errors[:longDescription].any?
-		#assert project.errors[:endDate].any?
-		#assert project.errors[:startDate].any?
+		assert project.errors[:startDate].any?
+		assert project.errors[:endDate].any?
 		assert project.errors[:fundingGoal].any?
 		
 		#Test these two columns have been initialized
@@ -47,5 +47,36 @@ class ProjectTest < ActiveSupport::TestCase
 
 		project.fundingGoal = Project.MIN_FUNDING_GOAL + 0.11
 		assert project.valid?
+	end
+
+	test "valid start and end date" do
+		project = projects(:goggles)
+		assert project.valid?, "Project wasn't valid to begin with, check project fixture"
+	
+		#Test startDate >= Today
+		project.startDate = Date.today - 1.days
+		project.endDate = Date.today + 10.days
+		assert project.invalid?
+
+		#Test startDate < Today + 1 month
+		project.startDate = Date.today + 2.months
+		project.endDate = project.startDate + 2.days
+		assert project.invalid?
+
+		#Test endDate >= startDate + 1 Day
+		project.endDate = Date.today + 1.days
+		project.startDate = project.endDate
+		assert project.invalid?
+		
+		project.endDate = project.startDate - 1.days
+		assert project.invalid?
+
+		#Test endDate <= startDate + 1 month
+		project.endDate = Date.today + 1.months
+		project.startDate = Date.today
+		assert project.valid?
+
+		project.endDate += 1.days
+		assert project.invalid?
 	end
 end
