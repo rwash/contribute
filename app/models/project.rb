448328449fbd875c7f4a8ae15e437dd1a64b8de3
Mitchell Thelen <mthelen@socialmedia.cas.msu.edu>
2012-01-27 13:33:35 -0500

    changed fixture, finished model tests (besides start + end date stuff)

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,19 +2,19 @@ class Project < ActiveRecord::Base
 	has_one :category
 
 	#Validation constants
-	MAX_NAME_LENGTH = 75
-	MAX_SHORT_DESC_LENGTH = 200
-	MAX_LONG_DESC_LENGTH = 1000
-	MIN_FUNDING_GOAL = 500	
-	MAX_FUNDING_GOAL = 250000
+	def self.MAX_NAME_LENGTH; 75; end
+	def self.MAX_SHORT_DESC_LENGTH; 200; end
+	def self.MAX_LONG_DESC_LENGTH; 1000; end
+	def self.MIN_FUNDING_GOAL; 500; end
+	def self.MAX_FUNDING_GOAL; 250000; end
 	#todo: dates
 
 	#Validation implementation
-	validates :name, :presence => true, :uniqueness => true, :length => {:maximum => MAX_NAME_LENGTH}
-	validates :shortDescription, :presence => true, :length => {:maximum => MAX_SHORT_DESC_LENGTH}
-	validates :longDescription, :presence => true, :length => {:maximum => MAX_LONG_DESC_LENGTH}
+	validates :name, :presence => true, :uniqueness => true, :length => {:maximum => Project.MAX_NAME_LENGTH}
+	validates :shortDescription, :presence => true, :length => {:maximum => Project.MAX_SHORT_DESC_LENGTH}
+	validates :longDescription, :presence => true, :length => {:maximum => Project.MAX_LONG_DESC_LENGTH}
 
-	validates :fundingGoal, :presence => true, :format => { :with => /^\d+??(?:\.\d{0,2})?$/ }, :numericality => {:greater_than_or_equal_to => MIN_FUNDING_GOAL, :less_than_or_equal_to => MAX_FUNDING_GOAL}
+	validates :fundingGoal, :presence => true, :format => { :with => /^\d+??(?:\.\d{0,2})?$/ }, :numericality => {:greater_than_or_equal_to => Project.MIN_FUNDING_GOAL, :less_than_or_equal_to => Project.MAX_FUNDING_GOAL}
 	validates :created_at, :presence => true
 	validates :active, :presence => true
 	#todo: dates
--- test/unit/project_test.rb
+++ test/unit/project_test.rb
@@ -23,5 +23,29 @@ class ProjectTest < ActiveSupport::TestCase
 		assert_equal "has already been taken", project.errors[:name].join('; ')
 	end
 
-	
+	test "valid funding goal" do
+		project = projects(:goggles)
+		assert project.valid?, "Project wasn't valid to begin with, check project fixture"
+		
+		project.fundingGoal = Project.MIN_FUNDING_GOAL - 1
+		assert project.invalid?
+
+		project.fundingGoal = Project.MAX_FUNDING_GOAL + 1
+		assert project.invalid?
+
+		project.fundingGoal = -1
+		assert project.invalid?
+
+		project.fundingGoal = Project.MIN_FUNDING_GOAL + 0.1234
+		assert project.invalid?
+
+		project.fundingGoal = Project.MIN_FUNDING_GOAL + 1
+		assert project.valid?
+
+		project.fundingGoal = Project.MAX_FUNDING_GOAL - 1
+		assert project.valid?
+
+		project.fundingGoal = Project.MIN_FUNDING_GOAL + 0.11
+		assert project.valid?
+	end
 end
