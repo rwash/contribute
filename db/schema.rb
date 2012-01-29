5e1031d6be231ad2f9250b1d8df3c9ad7105b9a8
rockwe34 <rockwe34@socialmedia.cas.msu.edu>
2012-01-29 18:57:45 -0500

    Removing start date from projects in migration and schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120127191117) do
+ActiveRecord::Schema.define(:version => 20120125014812) do
 
   create_table "categories", :force => true do |t|
     t.string   "shortDescription"
@@ -30,7 +30,6 @@ ActiveRecord::Schema.define(:version => 20120127191117) do
     t.integer  "categoryId"
     t.decimal  "fundingGoal",      :precision => 8, :scale => 2
     t.boolean  "active"
-    t.date     "startDate"
   end
 
 end
