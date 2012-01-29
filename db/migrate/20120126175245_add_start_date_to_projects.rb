5da284336d3d172f94dece0c7569ec33de6f1bba
rockwe34 <rockwe34@socialmedia.cas.msu.edu>
2012-01-29 18:57:45 -0500

    Removing start date from projects in migration and schema

--- db/migrate/20120126175245_add_start_date_to_projects.rb
+++
@@ -1,9 +0,0 @@
-class AddStartDateToProjects < ActiveRecord::Migration
-  def self.up
-    add_column :projects, :startDate, :datetime
-  end
-
-  def self.down
-    remove_column :projects, :startDate
-  end
-end
