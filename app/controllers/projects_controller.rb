a2731b3b148b77aecdd1f099af9484d6274d4078
rockwe34 <rockwe34@socialmedia.cas.msu.edu>
2012-01-30 17:10:31 -0500

    Edit and Index changes. Changing default date/time presentation. Other small view tweaks.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,19 +1,8 @@
 class ProjectsController < InheritedResources::Base
 	actions :all, :except => [ :destroy ]
 
-	def show
-		begin
-			show!
-		rescue ActiveRecord::RecordNotFound
-			redirect_to projects_url, notice: 'Invalid Project'
-		end
-	end
-
-	def edit
-		begin
-			edit!
-		rescue ActiveRecord::RecordNotFound
-			redirect_to projects_url, notice: 'Invalid Project'
-		end
+	def index
+		@projects = Project.limit(10).where("active = 1").order("endDate ASC")
+		index!
 	end
 end
