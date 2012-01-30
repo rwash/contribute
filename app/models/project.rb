f594aa6103d4719c5ec1ff383aea674456b95716
rockwe34 <rockwe34@socialmedia.cas.msu.edu>
2012-01-29 20:27:53 -0500

    Updating project model to remove improper validation

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,8 +5,7 @@ class Project < ActiveRecord::Base
 	def self.MAX_NAME_LENGTH; 75; end
 	def self.MAX_SHORT_DESC_LENGTH; 200; end
 	def self.MAX_LONG_DESC_LENGTH; 1000; end
-	def self.MIN_FUNDING_GOAL; 500; end
-	def self.MAX_FUNDING_GOAL; 250000; end
+	def self.MIN_FUNDING_GOAL; 5; end
 	#todo: dates
 
 	#Validation implementation
@@ -14,11 +13,10 @@ class Project < ActiveRecord::Base
 	validates :shortDescription, :presence => true, :length => {:maximum => Project.MAX_SHORT_DESC_LENGTH}
 	validates :longDescription, :presence => true, :length => {:maximum => Project.MAX_LONG_DESC_LENGTH}
 
-	validates :fundingGoal, :presence => true, :format => { :with => /^\d+??(?:\.\d{0,2})?$/ }, :numericality => {:greater_than_or_equal_to => Project.MIN_FUNDING_GOAL, :less_than_or_equal_to => Project.MAX_FUNDING_GOAL}
+	validates :fundingGoal, :presence => true, :format => { :with => /^\d+??(?:\.\d{0,2})?$/ }, :numericality => {:greater_than_or_equal_to => Project.MIN_FUNDING_GOAL}
 	validates :created_at, :presence => true
 	validates :active, :presence => true
 
-	validates :startDate, :presence => true
 	validates :endDate, :presence => true
 	validate :valid_start_end_date
 
@@ -26,15 +24,5 @@ class Project < ActiveRecord::Base
 		super
 		self.active = true
 		self.created_at = Date.today
-	end
-
-	def valid_start_end_date
-		if !startDate || !endDate
-			return
-		elsif startDate < Date.today || startDate > Date.today + 1.months
-			errors.add(:startDate, "must be on or within a month from today")
-		elsif endDate < startDate + 1.days || endDate > startDate + 1.month
-			errors.add(:endDate, "must be greater or equal to the day after the start day and less or equal to a month within the start date")
-		end
 	end
 end
