d18462ea3dc6125f9890c633bdfffe45f82e7697
Mitchell Thelen <mthelen@socialmedia.cas.msu.edu>
2012-01-31 14:50:34 -0500

    more cleanup

--- test/functional/project_controller_test.rb
+++ test/functional/project_controller_test.rb
@@ -2,20 +2,11 @@ require 'test_helper'
 
 class ProjectsControllerTest < ActionController::TestCase
 
-	NON_EXISTENT_PROJECT_ID = 9999
-
 	setup do
 		@project = get_valid_project(:goggles)
 	end
 
 	#SHOW TESTING
-	test "show_redirect_on_invalid_id" do
-		assert_equal(Project.find_by_id(NON_EXISTENT_PROJECT_ID), nil, "Project exists, test is unusable, change NON_EXISTENT_PROJECT_ID")
-		get(:show, {'id' => NON_EXISTENT_PROJECT_ID})
-		assert_redirected_to projects_path
-				assert_match(flash[:notice], /(.*)Invalid(.*)/, "Flash notice did not say '\Invalid\'.  Did you change the message?")
-	end
-
 	test "show_on_valid_id" do
 		get(:show, :id => @project.id)
 		assert_response :success
@@ -58,11 +49,5 @@ class ProjectsControllerTest < ActionController::TestCase
 		@project.fundingGoal = -1
 		put :update, id: @project.to_param, project: @project.attributes
 		assert_select "h2", {:text => /(.*)error(.*)/}, "heading does not contain errors" 
-	end
-
-	test "edit_should_redirect_on_invalid_url" do
-		get :edit, id: NON_EXISTENT_PROJECT_ID
-		assert_redirected_to projects_path
-		assert_match(flash[:notice], /(.*)Invalid(.*)/, "Flash notice did not say '\Invalid\'.  Did you change the message?")
 	end
 end
