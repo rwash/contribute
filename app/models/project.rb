8a0201306939e19e2828c8e958fc10827183dc35
rockwe34 <rockwe34@socialmedia.cas.msu.edu>
2012-01-31 14:16:50 -0500

    Removing datepicker on start date field which does not exist. Removing todo line from model. Undoing display change in environment.rb that breaks dates. See Bug #38 for more info.

--- app/models/project.rb
+++ app/models/project.rb
@@ -7,7 +7,6 @@ class Project < ActiveRecord::Base
 	def self.MAX_SHORT_DESC_LENGTH; 200; end
 	def self.MAX_LONG_DESC_LENGTH; 1000; end
 	def self.MIN_FUNDING_GOAL; 5; end
-	#todo: dates
 
 	#Validation implementation
 	validates :name, :presence => true, :uniqueness => true, :length => {:maximum => Project.MAX_NAME_LENGTH}
