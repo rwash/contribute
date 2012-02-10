e25f310222f80cb4a1823cfa871f02780062ad0e
rockwe34 <rockwe34@socialmedia.cas.msu.edu>
2012-02-10 17:42:42 -0500

    Added name and location fields.

--- db/migrate/20120210150755_devise_create_users.rb
+++ db/migrate/20120210150755_devise_create_users.rb
@@ -4,6 +4,8 @@ class DeviseCreateUsers < ActiveRecord::Migration
       ## Database authenticatable
  			t.string :email,              :null => false, :default => ""
 	 		t.string :encrypted_password, :null => false, :default => ""
+			t.string :name, :null => false, :default => ""
+			t.string :location, :null => true
 
 			## Recoverable
 			t.string   :reset_password_token
