5c98704c7789d72cad9d8a4143956386d9a97d58
rockwe34 <rockwe34@socialmedia.cas.msu.edu>
2012-02-10 17:42:42 -0500

    Added name and location fields.

--- db/schema.rb
+++ db/schema.rb
@@ -39,6 +39,8 @@ ActiveRecord::Schema.define(:version => 20120210150755) do
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
     t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "name",                   :default => "", :null => false
+    t.string   "location"
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
