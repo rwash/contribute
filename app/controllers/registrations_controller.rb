f70bde8699f4e245276d2fe838143ff5b95b8482
rockwe34 <rockwe34@socialmedia.cas.msu.edu>
2012-02-14 16:43:11 -0500

    Moving show into registrations controller and killing remnants of user controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,10 @@
 class RegistrationsController < Devise::RegistrationsController
+	def show
+		@user = User.find_by_id(params[:id])
+	end
+
 	protected
 		def after_update_path_for(resource)
-			puts "THIS HAPPENED"
 			user_path(resource)
 		end
 end
