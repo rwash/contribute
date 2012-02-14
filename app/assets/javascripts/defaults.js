97cb61e77df745941473f4ae027bb542ee341eb6
rockwe34 <rockwe34@socialmedia.cas.msu.edu>
2012-02-13 21:45:40 -0500

    Adding a comment to project_controller. Adding vertical progress bar in the projects partial with accompanying JS and CSS.

--- app/assets/javascripts/defaults.js
+++ app/assets/javascripts/defaults.js
@@ -1,3 +1,13 @@
 $(function (){
 	$('#DatePickerEndDate').datepicker();
+
+	// Parse the vlaue inside the div tag. Set the value of the progress bar equal to that.
+	// Expand the width so it fills width-wise, set the height so it's like a vertical bar
+	$('.progressBar').each(function() {
+		var value = parseInt($(this).text());
+		value = value + 1;
+		$(this).empty().progressbar({value: value});
+		$( '.ui-progressbar-value' ).width('100%');
+		$( '.ui-progressbar-value' ).height(value + '%');
+	});
 });
