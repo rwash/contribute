39f6b751589c284d408b85854a82f30f23358e1c
rockwe34 <rockwe34@msu.edu>
2012-02-21 14:21:32 -0500

    Fixing authorization messages for CanCan

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
  protect_from_forgery
 
 	rescue_from CanCan::AccessDenied do |exception|
-		redirect_to root_url, :alert => 'You must be signed in to perform this action'
+		redirect_to root_url, :alert => exception.message
 	end
 
  	def set_current_project_by_name
