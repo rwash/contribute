c7740422a9f36ad4a67e75e2f0547a948c9f53fa
rockwe34 <rockwe34@msu.edu>
2012-03-06 15:14:27 -0500

    Making name uniqueness on project case insensitive. Adding uniqueness to category. Adding *.swp to gitignore

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,7 @@
 class Category < ActiveRecord::Base
 	belongs_to :project
 
+	validates :short_description, :uniqueness => { :case_sensitive => false }
+
 	attr_accessible :short_description, :long_description
 end
