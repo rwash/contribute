baf370fc8031ce08f8137d9ce9a04abb0152ab82
rockwe34 <rockwe34@msu.edu>
2012-03-20 14:43:25 -0400

    Adding update rule back to can can. Fixing formatting on user show and project show.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -10,8 +10,9 @@ class Ability
 		can :update, Project, :active => true, :user_id => user.id
 		can :save, Project
 
-		#If the user is anonymous, return true,
-		#else, make sure the user isn't a project owner and doesn't have a contribution already
+		#Contributions
+		# If the user is anonymous, return true,
+		# else, make sure the user isn't a project owner and doesn't have a contribution already
 		can :contribute, Project do |project|
 			if !user.id.nil?
 				project.user_id != user.id and project.contributions.find_by_user_id(user.id).nil?
@@ -19,10 +20,13 @@ class Ability
 				true #He can contribute, because he's anonymous
 			end
 		end
-
-		#If the user is logged in, doesn't own the project,  and has a contribution on this project, they can edit
+		# If the user is logged in, doesn't own the project,  and has a contribution on this project,
+		# they can edit
 		can :edit_contribution, Project do |project|
-			!user.id.nil? and project.contributions.find_by_user_id(user.id).nil?
+			!user.id.nil? and !project.contributions.find_by_user_id(user.id).nil?
 		end
- end
+		can :update, Contribution do |contribution|
+			user.id.nil? and contribution.user_id == user.id
+		end
+	end
 end
