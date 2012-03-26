7c3422fd5d6d1cad346b66bb55bc00ee9b452398
rockwe34 <rockwe34@msu.edu>
2012-03-26 13:33:24 -0400

    Fixed comment typo. Renamed variable for readability. Progress bar is still created without ui-progressbar-value.

--- app/assets/javascripts/defaults.js
+++ app/assets/javascripts/defaults.js
@@ -1,15 +1,15 @@
 $(function (){
 	$('#DatePickerEndDate').datepicker();
 
-	// Parse the vlaue inside the div tag. Set the value of the progress bar equal to that.
+	// Parse the value inside the div tag. Set the value of the progress bar equal to that.
 	// Expand the width so it fills width-wise, set the height so it's like a vertical bar
 	$('.progressBar').each(function() {
-		var value = parseInt($(this).text());
-		value = value + 1; //1 looks like 0. Pad it a little.
-		if(value > 100) { value = 100; }
+		var pbValue = parseInt($(this).text());
+		pbValue = pbValue + 1; //1 looks like 0. Pad it a little.
+		if(pbValue > 100) { pbValue = 100; }
 
-		$(this).empty().progressbar({value: value});
+		$(this).progressbar({ value: pbValue });
 		$(this).find('.ui-progressbar-value').width('100%');
-		$(this).find('.ui-progressbar-value').height(value + '%');
+		$(this).find('.ui-progressbar-value').height(pbValue + '%');
 	});
 });
