d4316b6b79f1cc243769a37d51e9da9d5c97ed5e
rockwe34 <rockwe34@msu.edu>
2012-03-26 13:51:45 -0400

    Resolving what I believe to be a name collision on progressBar. Hoping it will stop the intermittent non-loading of ui-progressbar-value. Making contribution percentage an int as well.

--- app/assets/javascripts/defaults.js
+++ app/assets/javascripts/defaults.js
@@ -3,12 +3,12 @@ $(function (){
 
 	// Parse the value inside the div tag. Set the value of the progress bar equal to that.
 	// Expand the width so it fills width-wise, set the height so it's like a vertical bar
-	$('.progressBar').each(function() {
+	$('.contributionBar').each(function() {
 		var pbValue = parseInt($(this).text());
 		pbValue = pbValue + 1; //1 looks like 0. Pad it a little.
 		if(pbValue > 100) { pbValue = 100; }
 
-		$(this).progressbar({ value: pbValue });
+		$(this).empty().progressbar({ value: pbValue });
 		$(this).find('.ui-progressbar-value').width('100%');
 		$(this).find('.ui-progressbar-value').height(pbValue + '%');
 	});
