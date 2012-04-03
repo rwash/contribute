f364acbbfaaec1a1fe1451be14d345995f0425c5
rockwe34 <rockwe34@msu.edu>
2012-04-03 14:58:38 -0400

    Adding e-mail to the cancellations rake task.

--- app/mailers/email_manager.rb
+++ app/mailers/email_manager.rb
@@ -1,11 +1,13 @@
 class EmailManager < ActionMailer::Base
   default :from => Rails.application.config.from_address
 
+	@@admin_address = Rails.application.config.admin_address
+
 	def add_project(user, project)
 		@user = user
 		@project = project
 
-		mail(:to => @user.email, :subject => "Contribute: #{@project.name} has been created")
+		mail(:to => @user.email, :subject => "#{@project.name} has been created")
 	end
 
 	def contribute_to_project(user, contribution)
@@ -13,7 +15,7 @@ class EmailManager < ActionMailer::Base
 		@contribution = contribution
 		@project = @contribution.project
 
-		mail(:to => @user.email, :subject => "Contribute: Your contribution to #{@project.name}")
+		mail(:to => @user.email, :subject => "Your contribution to #{@project.name}")
 	end
 
 	def edit_contribution(user, old_contribution, new_contribution)
@@ -22,6 +24,12 @@ class EmailManager < ActionMailer::Base
 		@new_contribution = new_contribution
 		@project = @old_contribution.project
 
-		mail(:to => @user.email, :subject => "Contribute: Your edited contribution to #{@project.name}")
+		mail(:to => @user.email, :subject => "Your edited contribution to #{@project.name}")
+	end
+
+	def failed_cancellations(contributions_waiting_cancellation)
+		@contributions = contributions_waiting_cancellation
+
+		mail(:to => @@admin_address, :subject => "Contributions waiting for cancellation for more than 3 days")
 	end
 end
--- lib/tasks/contributions_waiting_cancellation.rake
+++ lib/tasks/contributions_waiting_cancellation.rake
@@ -3,12 +3,18 @@
 namespace :contribute do
 	task :contributions_waiting_cancellation => :environment do
 		contributions_to_process = Contribution.where("waiting_cancellation > 0")
+		contributions_still_failing = Array.new
+
 
 		contributions_to_process.each do |contribution|
 			contribution.cancel
 			if contribution.waiting_cancellation > 3
-				#TODO: send an e-mail to the admin to check on this
+				contributions_still_failing.push contribution	
 			end
+		end
+	
+		if contributions_still_failing.size > 0
+			EmailManager.failed_cancellations(contributions_still_failing).deliver
 		end
 	end
 end
