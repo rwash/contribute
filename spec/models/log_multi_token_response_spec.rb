5b7d572b4533a360e99e65dbcd2060136f2741e5
Mitchell Thelen <mthelen@socialmedia.cas.msu.edu>
2012-04-02 23:59:33 -0400

    fixed my spec screwups

--- spec/models/log_multi_token_response_spec.rb
+++ spec/models/log_multi_token_response_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
 
-describe LogMultiTokenResponse do
+describe Logging::LogMultiTokenResponse do
   pending "add some examples to (or delete) #{__FILE__}"
 end
--- spec/models/log_pay_response_spec.rb
+++ spec/models/log_pay_response_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
 
-describe LogPayResponse do
+describe Logging::LogPayResponse do
   pending "add some examples to (or delete) #{__FILE__}"
 end
