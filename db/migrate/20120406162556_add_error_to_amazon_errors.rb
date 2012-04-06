3331fbdd013d7049ce990f9d1c66424e17a54dab
Mitchell Thelen <mthelen@socialmedia.cas.msu.edu>
2012-04-06 12:34:15 -0400

    added amazon_errors table and seeding

---
+++ db/migrate/20120406162556_add_error_to_amazon_errors.rb
@@ -0,0 +1,9 @@
+class AddErrorToAmazonErrors < ActiveRecord::Migration
+  def up
+		add_column :amazon_errors, :error, :string
+  end
+
+  def down
+		remove_column :amazon_errors, :error
+  end
+end
