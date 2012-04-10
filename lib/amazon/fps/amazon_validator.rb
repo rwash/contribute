e8c65b4bf882c1360aa4bdeabe6101f327465627
rockwe34 <rockwe34@msu.edu>
2012-04-10 15:19:31 -0400

    Making some semantic and boolean changes

--- lib/amazon/fps/amazon_validator.rb
+++ lib/amazon/fps/amazon_validator.rb
@@ -6,21 +6,13 @@ module FPS
 #Disclaimer: ruby made me add if <expression> return true else return false; otherwise I received a void value expression?
 class AmazonValidator
 	#if the contribution in the session controller is not available, there is no tokenID returned, the status code was not successful, or the signature could not be verfied	
-	def self.invalid_multi_token_response?(url, session, params)
-		if session[:contribution].nil? or params[:tokenID].nil? or invalid_multi_token_status?(params[:status]) or !valid_cbui_response?(params, url)
-			return true
-		else
-			return false
-		end
+	def self.valid_multi_token_response?(url, session, params)
+		return (!session[:contribution].nil? and !params[:tokenID].nil? and valid_multi_token_status?(params[:status]) and valid_cbui_response?(params, url))
 	end
 
 	#if the project in the session controller is not available, there is no tokenID return, the status code is not a successful one, or the signature could not be verified
-	def self.invalid_recipient_response?(url, session, params)
-		if session[:project].nil? or params[:tokenID].nil? or params[:status] != "SR" or !valid_cbui_response?(params, url)
-			return true
-		else
-			return false
-		end
+	def self.valid_recipient_response?(url, session, params)
+		return (!session[:project].nil? and !params[:tokenID].nil? and params[:status] == "SR" and valid_cbui_response?(params, url))
 	end
 
 	#if the response contains errors or the transaction status was not a success
@@ -70,11 +62,11 @@ protected
 	end
 
 	#these are acceptable status codes for the payment method
-	def self.invalid_multi_token_status?(status)
+	def self.valid_multi_token_status?(status)
 		if status != "SA" and status != "SB" and status != "SC"
-			return true
-		else
 			return false
+		else
+			return true
 		end
 	end
 end
