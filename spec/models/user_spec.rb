176f8fbf5fda9d04cdc4970f0f80f5ece05a5104
rockwe34 <rockwe34@msu.edu>
2012-04-13 17:29:15 -0400

    Adding the only non-Devise user validation test. Adding a task to make rake db:test:prepare load the seeds as well. Killing a temp file. Working on moving permissions tests to requests instead of controller but running into lots of trouble.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,24 @@
 require 'spec_helper'
 
 describe User do
+	describe 'valid case' do
+		before(:all) do
+			@user = FactoryGirl.create(:user)
+		end
+
+		after(:all) do
+			@user.delete
+		end
+
+		it 'checks validity to make the before run :)' do
+			assert_not_nil @user
+		end
+	end
+
+	describe 'name' do
+		it 'is required' do
+			user = FactoryGirl.build(:user, :name => '')
+			assert !user.save, "Incorrectly saved user with blank name"
+		end
+	end
 end
