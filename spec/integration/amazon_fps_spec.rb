3818b7d5aee941d17931ba994446494465ce9e0e
Mitchell Thelen <mthelen@socialmedia.cas.msu.edu>
2012-04-23 22:19:00 -0400

    committed fps tests

--- spec/integration/amazon_fps_spec.rb
+++ spec/integration/amazon_fps_spec.rb
@@ -6,9 +6,10 @@ class AmazonFpsTesting
 		fixtures :users
 
 		before :all do
-			@project = FactoryGirl.create(:project)
-
 			Capybara.default_driver = :selenium
+
+			@test1_project = FactoryGirl.create(:project)
+			@test2_project = FactoryGirl.create(:project2)
 
 			@headless = Headless.new
 			@headless.start
@@ -19,20 +20,45 @@ class AmazonFpsTesting
 			Contribution.delete_all
 		end
 
-		it "succeed on pay request" do
+		it "succeed on pay request and check transaction status" do
 			@contribution = generate_contribution(
 				'thelen56@msu.edu', #contribution login
 				'aaaaaa',
 				'contribute_testing@hotmail.com', #amazon login
 				'testing',
-				@project, #the project to contribute to
+				@test1_project, #the project to contribute to
 				100) #the amount
+
+			request = Amazon::FPS::PayRequest.new(@contribution.payment_key, @contribution.project.payment_account_id, @contribution.amount)
+			response = request.send()
+
+			puts response
+			response['Errors'].should be_nil
+			response['PayResult']['TransactionStatus'].should_not be_nil
+	
+			transaction_id = response['PayResult']['TransactionId']
+			transaction_id.should_not be_nil
+
+			request = Amazon::FPS::GetTransactionStatusRequest.new(transaction_id)
+			response = request.send()
+
+			response['Errors'].should be_nil
+			response['GetTransactionStatusResult']['TransactionStatus'].should_not be_nil
 		end
 
 		it "succeed on cancel token request" do
-		end
+			@contribution = generate_contribution(
+				'thelen56@msu.edu', #contribution login
+				'aaaaaa',
+				'contribute_testing@hotmail.com', #amazon login
+				'testing',
+				@test2_project, #the project to contribute to
+				100) #the amount
 
-		it "succeed on get transaction request" do
+			request = Amazon::FPS::CancelTokenRequest.new(@contribution.payment_key)
+			response = request.send()
+
+			response['Errors'].should be_nil
 		end
 	end
 end
