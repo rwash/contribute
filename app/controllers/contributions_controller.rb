ec52f66ca001995adac361d06ce297b8e9844a8b
rockwe34 <rockwe34@msu.edu>
2012-04-27 17:18:15 -0400

    Fixing a couple confirmed bugs

--- app/controllers/contributions_controller.rb
+++ app/controllers/contributions_controller.rb
@@ -137,7 +137,7 @@ class ContributionsController < ApplicationController
 
 protected
 	def validate_project
-		if !@project.active
+		if !@project.active or !@project.confirmed
 			flash[:alert] = ERROR_STRING
 			return redirect_to root_path	
 		end
