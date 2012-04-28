91df2817bf6dff7c2d1c77632f7f5c82caa56538
Mitchell Thelen <mthelen@socialmedia.cas.msu.edu>
2012-04-28 17:30:42 -0400

    finished project controller tests, might add one for save action later

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -62,4 +62,84 @@ describe ProjectsController do
 			end
 		end
 	end
+
+	describe "functional tests:" do
+		render_views
+
+		context "index action" do
+			it "should succeed" do
+				get "index"
+				response.should be_success
+			end
+		end
+
+		context "create action" do
+			before(:all) do
+				UUIDTools::UUID.stub(:random_create){}
+
+				@user = FactoryGirl.create(:user)
+				@user.confirm!
+			end
+
+			after(:all) do
+				@user.delete
+			end
+
+			it "should succeed for signed in user" do
+				sign_in @user
+				post 'create', :project => FactoryGirl.attributes_for(:project)
+
+				request = Amazon::FPS::RecipientRequest.new(save_project_url)
+				response.should redirect_to(request.url)
+			end
+
+			it "should fail for invalid project" do
+				sign_in @user
+				attributes = FactoryGirl.attributes_for(:project)
+				attributes[:funding_goal] = -5
+				post 'create', :project => attributes			
+	
+				response.should be_success
+				response.body.inspect.include?("error").should be_true
+				Project.find_by_name(attributes[:name]).should be_nil
+			end
+		end
+		
+		context "destroy action" do
+			before(:all) do
+				@project = FactoryGirl.create(:project)
+
+				@user = FactoryGirl.create(:user)
+				@user.confirm!
+
+				@project.user_id = @user.id
+				@project.save
+			end
+
+			after(:all) do
+				@project.delete unless @project.nil?
+				@user.delete
+			end
+
+			it "should succeed destroy" do
+				sign_in @user
+				delete :destroy, :id => @project.name
+
+				response.should redirect_to(root_path)
+				assert flash[:alert].include?("successfully deleted"), flash[:alert]
+			end
+
+			it "should handle failure" do
+				Project.any_instance.stub(:destroy) {false}
+
+				sign_in @user
+				delete :destroy, :id => @project.name
+
+				response.should redirect_to(project_path(@project))
+
+				puts 'flash', flash[:alert]
+				assert flash[:alert].include?("could not be deleted"), flash[:alert]
+			end
+		end
+	end
 end
