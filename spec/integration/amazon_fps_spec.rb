82ed8d1cf81717e43e2935fb10762a52a6f2307e
Mitchell Thelen <mthelen@socialmedia.cas.msu.edu>
2012-04-28 22:13:20 -0400

    almost 90% coverage, added tests for 100% coverage to the fps requests

--- spec/integration/amazon_fps_spec.rb
+++ spec/integration/amazon_fps_spec.rb
@@ -6,6 +6,9 @@ class AmazonFpsTesting
 		fixtures :users
 
 		before :all do
+			Project.delete_all
+			Contribution.delete_all
+
 			Capybara.default_driver = :selenium
 
 			@test1_project = FactoryGirl.create(:project)
@@ -18,6 +21,8 @@ class AmazonFpsTesting
 		after :all do
 			Project.delete_all
 			Contribution.delete_all
+		
+			delete_logs()
 		end
 
 		it "succeed on pay request and check transaction status" do
@@ -43,6 +48,8 @@ class AmazonFpsTesting
 
 			response['Errors'].should be_nil
 			response['GetTransactionStatusResult']['TransactionStatus'].should_not be_nil
+			Logging::LogPayResponse.find_by_TransactionId(transaction_id).should_not be_nil
+			Logging::LogGetTransactionResponse.find_by_TransactionId(transaction_id).should_not be_nil
 		end
 
 		it "succeed on cancel token request" do
@@ -58,7 +65,37 @@ class AmazonFpsTesting
 			response = request.send()
 
 			response['Errors'].should be_nil
+
+			log_cancel_request = Logging::LogCancelRequest.find_by_TokenId(@contribution.payment_key)
+			log_cancel_request.should_not be_nil
+			Logging::LogCancelResponse.find_by_log_cancel_request_id(log_cancel_request.id).should_not be_nil
+		end
+
+		it "handle bad pay request" do
+			request = Amazon::FPS::PayRequest.new('poop', 'poop', 50)
+			response = request.send()
+
+			log_pay_request = Logging::LogPayRequest.find_by_SenderTokenId('poop')
+			log_pay_request.should_not be_nil
+			assert_amazon_error(log_pay_request.id)
+		end
+
+		it "handle bad get transaction status request" do
+			request = Amazon::FPS::GetTransactionStatusRequest.new('poop')
+			response = request.send()
+
+			log_get_transaction_request = Logging::LogGetTransactionRequest.find_by_TransactionId('poop')
+			log_get_transaction_request.should_not be_nil
+			assert_amazon_error(log_get_transaction_request.id)
+		end
+
+		it "handle bad cancel token request" do
+			request = Amazon::FPS::CancelTokenRequest.new('poop')
+			response = request.send()
+
+			log_cancel_request = Logging::LogCancelRequest.find_by_TokenId('poop')
+			log_cancel_request.should_not be_nil
+			assert_amazon_error(log_cancel_request.id)
 		end
 	end
 end
-
