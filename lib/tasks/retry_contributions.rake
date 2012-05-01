9ee66a32218b1ca9a44e2f0289f8cdd29ec96733
rockwe34 <rockwe34@msu.edu>
2012-05-01 19:03:31 -0400

    Pulling rake tasks out into classes so they're easier to test. Adding project name link back into user show page, though it's breaking for me right now since contribution.project is nil...

--- lib/tasks/retry_contributions.rake
+++ lib/tasks/retry_contributions.rake
@@ -1,64 +1,7 @@
-require 'logger'
-
-logger = Logger.new('log/cron_log.log')
+require 'tasks/retry_contributions'
 
 namespace :contribute do
 	task :retry_contributions => :environment do
-		logger.info "#{Date.today}: Starting contributions_retry"
-
-		still_failing = Array.new
-
-		check_pending(still_failing, logger)
-		retry_pay(still_failing, logger)
-		retry_cancel(still_failing, logger)
-	
-		if still_failing.size > 0
-			EmailManager.failed_retries(still_failing).deliver
-		end
-	
-		logger.info "#{Date.today}: All contributions have been processed\n"
+		RetryContributions.run
 	end
-
-	def check_pending(fail_array, logger)
-		to_check_pending = Contribution.where("status = ?", ContributionStatus::PENDING)
-		logger.info "Found #{to_check_pending.size} contributions to check pending"
-	
-		to_check_pending.each do |contribution|
-			logger.info "Contribution with id #{contribution.id} is checking status"
-			contribution.update_status
-			# This value might need to be tweaked depending on how many e-mails it causes
-			if contribution.retry_count > 3
-				fail_array.push contribution
-			end
-		end
-	end
-
-	def retry_pay(fail_array, logger)
-		to_retry_pay = Contribution.where("status = ?", ContributionStatus::RETRY_PAY)
-		logger.info "Found #{to_retry_pay.size} contributions to retry pay"
-
-		to_retry_pay.each do |contribution|
-			logger.info "Contribution with id #{contribution.id} is retrying payment"
-			contribution.execute_payment
-			# This value might need to be tweaked depending on how many e-mails it causes
-			if contribution.retry_count > 3
-				fail_array.push contribution
-			end
-		end
-	end
-
-	def retry_cancel(fail_array, logger)
-		to_retry_cancel = Contribution.where("status = ?", ContributionStatus::RETRY_CANCEL)
-		logger.info "Found #{to_retry_cancel.size} contributions to retry cancel"
-
-		to_retry_cancel.each do |contribution|
-			logger.info "Contribution with id #{contribution.id} is retrying cancellation"
-			contribution.cancel
-			# This value might need to be tweaked depending on how many e-mails it causes
-			if contribution.retry_count > 3
-				fail_array.push contribution
-			end
-		end
-	end
-
 end
---
+++ lib/tasks/retry_contributions.rb
@@ -0,0 +1,64 @@
+# This is the ruby that is behind the retry_contributions rake task
+# Pulling it out makes it way easier to test
+require 'logger'
+
+class RetryContributions
+	@@logger = Logger.new('log/cron_log.log')
+  @@still_failing = Array.new
+
+	def self.run
+    @@logger.info "#{Date.today}: Starting contributions_retry"
+
+    check_pending
+    retry_pay
+    retry_cancel
+
+    if @@still_failing.size > 0
+      EmailManager.failed_retries(@@still_failing).deliver
+    end
+
+    @@logger.info "#{Date.today}: All contributions have been processed\n"
+  end
+
+  def self.check_pending
+    to_check_pending = Contribution.where("status = ?", ContributionStatus::PENDING)
+    @@logger.info "Found #{to_check_pending.size} contributions to check pending"
+
+    to_check_pending.each do |contribution|
+      @@logger.info "Contribution with id #{contribution.id} is checking status"
+      contribution.update_status
+      # This value might need to be tweaked depending on how many e-mails it causes
+      if contribution.retry_count > 3
+        @@still_failing.push contribution
+      end
+    end
+  end
+
+  def self.retry_pay
+    to_retry_pay = Contribution.where("status = ?", ContributionStatus::RETRY_PAY)
+    @@logger.info "Found #{to_retry_pay.size} contributions to retry pay"
+
+    to_retry_pay.each do |contribution|
+      @@logger.info "Contribution with id #{contribution.id} is retrying payment"
+      contribution.execute_payment
+      # This value might need to be tweaked depending on how many e-mails it causes
+      if contribution.retry_count > 3
+        @@still_failing.push contribution
+      end
+    end
+  end
+
+  def self.retry_cancel
+    to_retry_cancel = Contribution.where("status = ?", ContributionStatus::RETRY_CANCEL)
+    @@logger.info "Found #{to_retry_cancel.size} contributions to retry cancel"
+
+    to_retry_cancel.each do |contribution|
+      @@logger.info "Contribution with id #{contribution.id} is retrying cancellation"
+      contribution.cancel
+      # This value might need to be tweaked depending on how many e-mails it causes
+      if contribution.retry_count > 3
+        @@still_failing.push contribution
+      end
+    end
+  end
+end
