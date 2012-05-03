540717de85a460e8a795654492604bdf7c48d12c
rockwe34 <rockwe34@msu.edu>
2012-05-03 15:13:54 -0400

    Adding a new test and two other tests that don't work in a todo

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -6,4 +6,10 @@ FactoryGirl.define do
 		name 'Test Guy'
 		password 'testme'
   end
+
+  factory :user2, class: User do
+    email 'test2@test.com'
+		name 'Test Guy 2'
+		password 'testme'
+  end
 end
--- spec/integration/user_page_spec.rb
+++ spec/integration/user_page_spec.rb
@@ -41,6 +41,60 @@ class UserPageTesting
 			current_path.should == user_path(@user)	
 			find('div#userProfile').has_content?('The Hulk')
 		end
+
+		describe "redirect tests" do
+			after(:each) do
+				User.delete_all
+				Project.delete_all
+				Contribution.delete_all
+			end
+	
+			it "should create user successfully" do
+				@user = FactoryGirl.build(:user)
+
+				visit new_user_registration_path
+
+				fill_in 'user_name', :with => @user.name
+				fill_in 'user_email', :with => @user.email
+				fill_in 'user_password', :with => @user.password
+				fill_in 'user_password_confirmation', :with => @user.password
+
+				click_button 'Sign up'
+
+				@user = User.find_by_email(@user.email)
+				current_path.should == edit_user_registration_path(@user)
+			end
+
+#TODO: For some reason these two tests fail like the user still exists, though
+# it should be gone from the User.delete_all. WTF.
+#			it "should create user successfully and redirect to project page" do
+#				@user = FactoryGirl.build(:user)
+#
+#				@project = FactoryGirl.create(:project)
+#				visit project_path(@project)
+#			
+#				click_button 'Contribute to this project'
+#				
+#				click_link "Don't have an account yet? Sign up!"
+#
+#				fill_in 'user_name', :with => @user.name
+#				fill_in 'user_email', :with => @user.email
+#				fill_in 'user_password', :with => @user.password
+#				fill_in 'user_password_confirmation', :with => @user.password
+#
+#				click_button 'Sign up'
+#				
+#				current_path.should == project_path(@project)
+#			end
+#
+#			it "should redirect to edit on confirm" do
+#				@user = FactoryGirl.create(:user)
+#
+#				visit "/users/confirmation?confirmation_token=#{@user.confirmation_token}"
+#		
+#				current_path.should == edit_user_registration_path(@user)
+#			end
+		end
 	end
 end
 
