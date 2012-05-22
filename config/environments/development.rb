32d5226e49e40a95cc90e99c5d3f12640361e8fb
Jacob Wesorick <Jake@13-176-197.client.wireless.msu.edu>
2012-05-22 10:17:50 -0400

    Comments now have a limited depth that is shown. It is defined in config/enviroment.rb. Server has to be restarted when changed for it to take effect.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -49,5 +49,7 @@ Contribute::Application.configure do
 	config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {:address => "localhost", :port => 1025}
+  
+  COMMENTS_DEPTH = 3
 
 end
