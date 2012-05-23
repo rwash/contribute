666720873af1c0667e7b3b5014cd51fecee6d2f7
Jake Wesorick <Jake.Wesorick@gmail.com>
2012-05-23 12:35:52 -0400

    added comments resources.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
 	def format_date(date)
 		date.strftime('%m/%d/%Y')
 	end
+	
+  def logged_in?
+    !current_user.nil?
+  end
+  
+  def comment_owner(comment)
+   logged_in? && current_user.id == comment.userid
+  end
 end
