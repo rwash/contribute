91f8d2dc5a51fabbc2bb9d4c8c75d67a276bf37e
Jake Wesorick <Jake.Wesorick@gmail.com>
2012-05-29 11:52:49 -0400

    Added validation to comments for ID.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,6 +3,7 @@ class Comment < ActiveRecord::Base
 
   validates_presence_of :body
   validates_presence_of :user
+  validates_presence_of :commentable_id
 
   # NOTE: install the acts_as_votable plugin if you
   # want user to vote on the quality of comments.
