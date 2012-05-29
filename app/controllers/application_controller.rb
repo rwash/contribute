6c63c9fd7ac8214eac965dde73a23b6dfd008aab
Jake Wesorick <Jake.Wesorick@gmail.com>
2012-05-29 10:09:28 -0400

    Added helpers.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
  protect_from_forgery
 
+  helper_method :logged_in?, :comment_owner
+
 	rescue_from CanCan::AccessDenied do |exception|
 		if exception.action == :contribute
 			redirect_to exception.subject, :alert => exception.message
@@ -14,4 +16,12 @@ class ApplicationController < ActionController::Base
  	def set_current_project_by_name
 		@project = Project.find_by_name(params[:id])
 	end
+	
+	 def logged_in?
+    !current_user.nil?
+  end
+  
+  def comment_owner(comment)
+   logged_in? && current_user.id == comment.user_id
+  end
 end
