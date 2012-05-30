a5c3269a3b34c392b199b9fb77d6ef927f8fb01f
Sam Mills <millssa4@orithena.cas.msu.edu>
2012-05-30 12:39:04 -0400

    updates

--- app/controllers/updates_controller.rb
+++ app/controllers/updates_controller.rb
@@ -2,10 +2,7 @@ class UpdatesController < InheritedResources::Base
   def create
     @update = Update.new(params[:update])
     @project = Project.where(:id => params[:project_id]).first
-<<<<<<< HEAD
     @update.project_id = @project.id
-=======
->>>>>>> 8e07dbcd19c35d7adbe4e46c7905cff8a84e62e7
     
     if user_signed_in? && (@project.user_id == current_user.id)
       @update.user_id = current_user.id
