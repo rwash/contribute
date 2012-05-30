17d284d30609a267c0e5c7f0602215581dc1dd77
Sam Mills <millssa4@orithena.cas.msu.edu>
2012-05-30 12:09:19 -0400

    updates

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -82,7 +82,7 @@ class ProjectsController < InheritedResources::Base
     @comment.commentable_id = @project.id
     @comment_depth = 0
     
-    @new_update = Update.new(params[:update])
+    @update = Update.new(params[:update])
     @updates = Update.where(:project => @project).all
   end
 
