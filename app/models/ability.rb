a0e940b6e391bf7629a9f939378971de4e8ff13a
Jake Wesorick <wesoric6@msu.edu>
2012-06-06 17:43:59 -0400

    Made it so you cannot contribute to a project after the project has ended.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -21,7 +21,7 @@ class Ability
 		#Contributions
 		# Make sure the user isn't a project owner and doesn't have a contribution already
 		can :contribute, Project do |project|
-			!user.id.nil? and project.user_id != user.id and project.contributions.find_by_user_id(user.id).nil?
+			!user.id.nil? and project.user_id != user.id and project.contributions.find_by_user_id(user.id).nil? and project.end_date >= Date.today
 		end
 		# If the user is logged in, doesn't own the project,  and has a contribution on this project,
 		# they can edit
