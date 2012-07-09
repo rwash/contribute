1c666e2342560dfff13a351cc869a65bc25d96b0
Sam Mills <millssa4@orithena.cas.msu.edu>
2012-07-09 14:15:28 -0400

    changed image sized

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -16,7 +16,7 @@ class PictureUploader < CarrierWave::Uploader::Base
 
 	version :show do
 		begin
-			process :resize_to_fill => [200,200]
+			process :resize_to_fill => [600,400]
 		rescue
 			raise CarrierWave::ProcessingError
 		end
