09b28c14fa224c99d0d4098e0e22f8734e728e06
Sam Mills <millssa4@orithena.cas.msu.edu>
2012-07-09 13:30:43 -0400

    some changes

--- config/environment.rb
+++ config/environment.rb
@@ -11,6 +11,6 @@ THREAD_DEPTH = 16
 # if you want to add a new state be sure to add it to the end of the array. Also add it as a valid state in the project model valid_state method
 PROJ_STATES = ['unconfirmed', 'inactive', 'active', 'nonfunded', 'funded', 'canceled']
 
-YT_DEV_KEY = 'AI39si5wMYM5Q5hivTEsSGrkm6vWRMyE-oOhwgE4QNss55LmgvvugH4vDOd9REkSBXDJL3vUE5WzXyXcll0gcCW3sFbdpqmxYA'
-YT_USERNAME = 'Jake.Wesorick@gmail.com'
-YT_PASSWORD = 'Crazy4DoctorWhoMan'
+YT_DEV_KEY = 'AI39si66_SkYIHlMQg0fKZ3yAz64tPDEAbzNm1af9FqmXeMgH5tHFPtGgSsVcUMzpKCOhKliREq0l467cgS-WIgrgxPzlQlerw'
+YT_USERNAME = 'contributemsu@gmail'
+YT_PASSWORD = 'somethingcool'
