9cafcd642542e9187ba84de563d6daa8f25cb6fb
Grayson Wright <wright.grayson@gmail.com>
2013-01-07 11:09:54 -0500

    Cleaned up approval factory

--- spec/factories/approval.rb
+++ spec/factories/approval.rb
@@ -1,10 +1,12 @@
 # Read about factories at http://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-	factory :approval do
-		group_id 1
-		project_id 1
-		approved nil
-		reason nil
-	end
+  factory :approval do
+    # Associate a group and a project
+    group
+    project
+
+    approved nil
+    reason nil
+  end
 end
