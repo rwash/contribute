feed0762997c3170429c5276aefd910810106592
Grayson Wright <wright.grayson@gmail.com>
2013-01-07 18:12:52 -0500

    Cleaned up group specs

--- spec/controllers/groups_controller_spec.rb
+++ spec/controllers/groups_controller_spec.rb
@@ -3,22 +3,18 @@ require 'controller_helper'
 
 describe GroupsController do
   include Devise::TestHelpers
-  
+
+  let(:user) { Factory :user }
+
 	describe "functional tests:" do
 		render_views
-	
-		before(:all) do
-			@user = FactoryGirl.create(:user)
-			@user.confirm!
-		end
-	
-		after(:all) do
-			@user.delete
-		end
+
+    let(:owned_group) { Factory :group, admin_user: user }
+    let(:other_group) { Factory :group }
 		
 		context "creating groups" do
 			it "signed in user can create group" do
-				sign_in @user
+				sign_in user
 				post 'create', :group => FactoryGirl.attributes_for(:group)
 				flash[:notice].should include "Successfully created group."
 			end
@@ -30,41 +26,35 @@ describe GroupsController do
 			end
 			
 			it "admin can edit group" do
-				sign_in @user
-				@group = FactoryGirl.create(:group, :open => true, :admin_user_id => @user.id)
+				sign_in user
 				
-				get 'edit', :id => @group.id
+				get 'edit', :id => owned_group.id
 				response.should be_success
 			end
 			
 			it "non admin can not edit group" do
-				sign_in @user
-				@group = FactoryGirl.create(:group, :open => true, :admin_user_id => @user.id + 1)
+				sign_in user
 				
-				get 'edit', :id => @group.id
+				get 'edit', :id => other_group.id
 				flash[:alert].should include "You are not authorized to access this page."
 			end
 		end
 		
 		context "destroy groups" do
 			it "not signed in user cannot delete group" do
-				@group = FactoryGirl.create(:group, :open => true, :admin_user_id => 19)
-				
-				get 'destroy', :id => @group.id
+				get 'destroy', :id => other_group.id
 				flash[:alert].should include "You are not authorized to access this page."
 			end
 			it "signed in user cannot delete group it doesnt own" do
-				sign_in @user
-				@group = FactoryGirl.create(:group, :open => true, :admin_user_id => @user.id + 1)
+				sign_in user
 				
-				get 'destroy', :id => @group.id
+				get 'destroy', id: other_group.id
 				flash[:alert].should include "You are not authorized to access this page."
 			end
 			it "signed in user can delete groups it is admin of" do
-				sign_in @user
-				@group = FactoryGirl.create(:group, :open => true, :admin_user_id => @user.id)
+				sign_in user
 				
-				get 'destroy', :id => @group.id
+				get 'destroy', :id => owned_group.id
 				response.should redirect_to(groups_path)
 			end
 		end
@@ -74,80 +64,73 @@ describe GroupsController do
 		#
 		
 		context "adding projects to open group" do
-			before(:all) do
-				@group = FactoryGirl.create(:group, :open => true, :admin_user_id => @user.id)
-			end
-			
-			after(:all) do
-				@group.delete
-			end
 			
 			it 'can add unconfirmed project to group' do
-				sign_in @user
-				@project = FactoryGirl.create(:project, :state => 'unconfirmed', :user_id => @user.id)
+				sign_in user
+				project = FactoryGirl.create(:project, :state => 'unconfirmed', :user_id => user.id)
 				
-				post 'submit_add', :id => @group.id, :project_id => @project.id
+				post 'submit_add', :id => owned_group.id, :project_id => project.id
 				
-				response.should redirect_to(@group)
+				response.should redirect_to(owned_group)
 				flash[:notice].should include "Your project has been added to the group."
 			end
 			
 			it 'can add inactive project to group' do
-				sign_in @user
-				@project = FactoryGirl.create(:project, :state => 'inactive', :user_id => @user.id)
+				sign_in user
+				project = FactoryGirl.create(:project, :state => 'inactive', :user_id => user.id)
 				
-				post 'submit_add', :id => @group.id, :project_id => @project.id
+				post 'submit_add', :id => owned_group.id, :project_id => project.id
 				
-				response.should redirect_to(@group)
+				response.should redirect_to(owned_group)
 				flash[:notice].should include "Your project has been added to the group."
 			end
 			it 'can add active project to group' do
-				sign_in @user
-				@project = FactoryGirl.create(:project, :state => 'active', :user_id => @user.id)
+				sign_in user
+				project = FactoryGirl.create(:project, :state => 'active', :user_id => user.id)
 				
-				post 'submit_add', :id => @group.id, :project_id => @project.id
+				post 'submit_add', :id => owned_group.id, :project_id => project.id
 				
-				response.should redirect_to(@group)
+				response.should redirect_to(owned_group)
 				flash[:notice].should include "Your project has been added to the group."
 			end
 			
 			it 'can add funded project to group' do
-				sign_in @user
-				@project = FactoryGirl.create(:project, :state => 'funded', :user_id => @user.id)
+				sign_in user
+				project = FactoryGirl.create(:project, :state => 'funded', :user_id => user.id)
 				
-				post 'submit_add', :id => @group.id, :project_id => @project.id
+				post 'submit_add', :id => owned_group.id, :project_id => project.id
 				
-				response.should redirect_to(@group)
+				response.should redirect_to(owned_group)
 				flash[:notice].should include "Your project has been added to the group."
 			end
 			
 			it 'can add nonfunded project to group' do
-				sign_in @user
-				@project = FactoryGirl.create(:project, :state => 'nonfunded', :user_id => @user.id)
+				sign_in user
+				project = FactoryGirl.create(:project, :state => 'nonfunded', :user_id => user.id)
 				
-				post 'submit_add', :id => @group.id, :project_id => @project.id
+				post 'submit_add', :id => owned_group.id, :project_id => project.id
 				
-				response.should redirect_to(@group)
+				response.should redirect_to(owned_group)
 				flash[:notice].should include "Your project has been added to the group."
 			end
 			
 			it 'can not add canceled project to group' do
-				sign_in @user
-				@project = FactoryGirl.create(:project, :state => 'canceled', :user_id => @user.id)
+				sign_in user
+				project = FactoryGirl.create(:project, :state => 'canceled', :user_id => user.id)
 				
-				post 'submit_add', :id => @group.id, :project_id => @project.id
+				post 'submit_add', :id => owned_group.id, :project_id => project.id
 				
-				response.should redirect_to(@group)
+				response.should redirect_to(owned_group)
 				flash[:error].should include "You cannot add a canceled project."
 			end
 			
-			it 'can not add group to a project twice' do
-				sign_in @user
-				@project = FactoryGirl.create(:project, :state => 'active', :user_id => @user.id)
-				@group.projects << @project
-				post 'submit_add', :id => @group.id, :project_id => @project.id
+			it 'can not add project to a group twice' do
+				sign_in user
+				project = FactoryGirl.create(:project, :state => 'active', :user_id => user.id)
+				owned_group.projects << project
+				post 'submit_add', :id => owned_group.id, :project_id => project.id
 				
-				response.should redirect_to(@group)
+				response.should redirect_to(owned_group)
 				flash[:error].should include "Your project is already in this group."
 			end
 		end
@@ -157,87 +140,80 @@ describe GroupsController do
 		#
 		
 		context "adding projects to closed group" do
-			before(:all) do
-				@admin = FactoryGirl.create(:user)
-				@group = FactoryGirl.create(:group, :open => false, :admin_user_id => @admin.id)
-			end
-			
-			after(:all) do
-				@group.delete
-				@admin.delete
-			end
+      let(:admin) { Factory :user }
+      let(:group) { Factory :group, open: false, admin_user: admin }
 			
 			it 'can add unconfirmed project to group' do
-				sign_in @user
-				@project = FactoryGirl.create(:project, :state => 'unconfirmed', :user_id => @user.id)
+				sign_in user
+				project = FactoryGirl.create(:project, :state => 'unconfirmed', :user_id => user.id)
 				
-				post 'submit_add', :id => @group.id, :project_id => @project.id
+				post 'submit_add', :id => group.id, :project_id => project.id
 				
-				response.should redirect_to(@group)
+				response.should redirect_to(group)
 				flash[:notice].should include "Your project has been submitted to the group admin for approval."
-				Approval.where(:project_id => @project.id, :group_id => @group.id, :approved => nil).first.should_not be_nil
+				Approval.where(:project_id => project.id, :group_id => group.id, :approved => nil).first.should_not be_nil
 			end
 			
 			it 'can add inactive project to group' do
-				sign_in @user
-				@project = FactoryGirl.create(:project, :state => 'inactive', :user_id => @user.id)
+				sign_in user
+				project = FactoryGirl.create(:project, :state => 'inactive', :user_id => user.id)
 				
-				post 'submit_add', :id => @group.id, :project_id => @project.id
+				post 'submit_add', :id => group.id, :project_id => project.id
 				
-				response.should redirect_to(@group)
+				response.should redirect_to(group)
 				flash[:notice].should include "Your project has been submitted to the group admin for approval."
-				Approval.where(:project_id => @project.id, :group_id => @group.id, :approved => nil).first.should_not be_nil
+				Approval.where(:project_id => project.id, :group_id => group.id, :approved => nil).first.should_not be_nil
 			end
 			it 'can add active project to group' do
-				sign_in @user
-				@project = FactoryGirl.create(:project, :state => 'active', :user_id => @user.id)
+				sign_in user
+				project = FactoryGirl.create(:project, :state => 'active', :user_id => user.id)
 				
-				post 'submit_add', :id => @group.id, :project_id => @project.id
+				post 'submit_add', :id => group.id, :project_id => project.id
 				
-				response.should redirect_to(@group)
+				response.should redirect_to(group)
 				flash[:notice].should include "Your project has been submitted to the group admin for approval."
-				Approval.where(:project_id => @project.id, :group_id => @group.id, :approved => nil).first.should_not be_nil
+				Approval.where(:project_id => project.id, :group_id => group.id, :approved => nil).first.should_not be_nil
 			end
 			
 			it 'can add funded project to group' do
-				sign_in @user
-				@project = FactoryGirl.create(:project, :state => 'funded', :user_id => @user.id)
+				sign_in user
+				project = FactoryGirl.create(:project, :state => 'funded', :user_id => user.id)
 				
-				post 'submit_add', :id => @group.id, :project_id => @project.id
+				post 'submit_add', :id => group.id, :project_id => project.id
 				
-				response.should redirect_to(@group)
+				response.should redirect_to(group)
 				flash[:notice].should include "Your project has been submitted to the group admin for approval."
-				Approval.where(:project_id => @project.id, :group_id => @group.id, :approved => nil).first.should_not be_nil
+				Approval.where(:project_id => project.id, :group_id => group.id, :approved => nil).first.should_not be_nil
 			end
 			
 			it 'can add nonfunded project to group' do
-				sign_in @user
-				@project = FactoryGirl.create(:project, :state => 'nonfunded', :user_id => @user.id)
+				sign_in user
+				project = FactoryGirl.create(:project, :state => 'nonfunded', :user_id => user.id)
 				
-				post 'submit_add', :id => @group.id, :project_id => @project.id
+				post 'submit_add', :id => group.id, :project_id => project.id
 				
-				response.should redirect_to(@group)
+				response.should redirect_to(group)
 				flash[:notice].should include "Your project has been submitted to the group admin for approval."
-				Approval.where(:project_id => @project.id, :group_id => @group.id, :approved => nil).first.should_not be_nil
+				Approval.where(:project_id => project.id, :group_id => group.id, :approved => nil).first.should_not be_nil
 			end
 			
 			it 'can not add canceled project to group' do
-				sign_in @user
-				@project = FactoryGirl.create(:project, :state => 'canceled', :user_id => @user.id)
+				sign_in user
+				project = FactoryGirl.create(:project, :state => 'canceled', :user_id => user.id)
 				
-				post 'submit_add', :id => @group.id, :project_id => @project.id
+				post 'submit_add', :id => group.id, :project_id => project.id
 				
-				response.should redirect_to(@group)
+				response.should redirect_to(group)
 				flash[:error].should include "You cannot add a canceled project."
 			end
 			
 			it 'can not add group to a project twice' do
-				sign_in @user
-				@project = FactoryGirl.create(:project, :state => 'active', :user_id => @user.id)
-				@group.projects << @project
-				post 'submit_add', :id => @group.id, :project_id => @project.id
+				sign_in user
+				project = FactoryGirl.create(:project, :state => 'active', :user_id => user.id)
+				group.projects << project
+				post 'submit_add', :id => group.id, :project_id => project.id
 				
-				response.should redirect_to(@group)
+				response.should redirect_to(group)
 				flash[:error].should include "Your project is already in this group."
 			end
 		end
--- spec/controllers/lists_controller_spec.rb
+++ spec/controllers/lists_controller_spec.rb
@@ -6,80 +6,71 @@ describe ListsController do
   
 	describe "functional tests:" do
 		render_views
+
+    let(:user) { Factory :user }
+    let(:user2) { Factory :user }
+		let(:group) { Factory :group, :admin_user => user }
+		let(:group2) { Factory :group, :admin_user => user2 }
 	
-		before(:all) do
-			@user = FactoryGirl.create(:user)
-			@user2 = FactoryGirl.create(:user)
-			@group = FactoryGirl.create(:group, :admin_user_id => @user.id)
-			@group2 = FactoryGirl.create(:group, :admin_user_id => @user2.id)
-		end
-	
-		after(:all) do
-			@user.delete
-			@user2.delete
-			@group.delete
-			@group2.delete
-		end
-		
 		context "creating lists" do
 			it "anyone can view users list" do
-				list = FactoryGirl.create(:list, :listable_id => @user.id, :listable_type => "User")
+				list = FactoryGirl.create(:list, :listable_id => user.id, :listable_type => "User")
 				get :show, :id => list.id
 				response.should be_success
 				list.delete
 			end
 			
 			it "anyone can view groups list" do
-				list = FactoryGirl.create(:list, :listable_id => @group.id, :listable_type => "Group")
+				list = FactoryGirl.create(:list, :listable_id => group.id, :listable_type => "Group")
 				get :show, :id => list.id
 				response.should be_success
 				list.delete
 			end
 			
 			it "user cannot destroy list it doesnt own" do
-				sign_in @user
+				sign_in user
 				
-				list = FactoryGirl.create(:list, :listable_id => @group2.id, :listable_type => "Group")
+				list = FactoryGirl.create(:list, :listable_id => group2.id, :listable_type => "Group")
 				get :destroy, :id => list.id
 				response.should redirect_to(root_path)
 			end
 			
 			it "user cannot destroy list it doesnt own (user)" do
-				sign_in @user
+				sign_in user
 				
-				list = FactoryGirl.create(:list, :listable_id => @user2.id, :listable_type => "User")
+				list = FactoryGirl.create(:list, :listable_id => user2.id, :listable_type => "User")
 				get :destroy, :id => list.id
 				response.should redirect_to(root_path)
 			end
 			
 			it "user can destroy list it does own" do
-				sign_in @user
+				sign_in user
 				
-				list = FactoryGirl.create(:list, :listable_id => @group.id, :listable_type => "Group")
+				list = FactoryGirl.create(:list, :listable_id => group.id, :listable_type => "Group")
 				get :destroy, :id => list.id
 				response.should redirect_to(list.listable)
 			end
 			
 			it "user can destroy list it does own (user)" do
-				sign_in @user
+				sign_in user
 				
-				list = FactoryGirl.create(:list, :listable_id => @user.id, :listable_type => "User")
+				list = FactoryGirl.create(:list, :listable_id => user.id, :listable_type => "User")
 				get :destroy, :id => list.id
 				response.should redirect_to(list.listable)
 			end
 		
 			it "user cannot edit list it doesnt own" do
-				sign_in @user
+				sign_in user
 				
-				list = FactoryGirl.create(:list, :listable_id => @group2.id, :listable_type => "Group")
+				list = FactoryGirl.create(:list, :listable_id => group2.id, :listable_type => "Group")
 				get :edit, :id => list.id
 				response.should redirect_to(root_path)
 			end
 			
 			it "user cannot edit list it doesnt own (user)" do
-				sign_in @user
+				sign_in user
 				
-				list = FactoryGirl.create(:list, :listable_id => @user2.id, :listable_type => "User")
+				list = FactoryGirl.create(:list, :listable_id => user2.id, :listable_type => "User")
 				get :edit, :id => list.id
 				response.should redirect_to(root_path)
 			end
