2a12a9147827a0009a3b3db3d3a6fe709146ce3d
Grayson Wright <wright.grayson@gmail.com>
2013-01-07 10:53:05 -0500

    Cleaned up comment factory

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,11 +1,15 @@
+# Generates a repeating segment of Lorem Ipsum text, of a specified length
+def lorem(length = 50)
+  lorem_ipsum = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  if length > lorem_ipsum.length
+    lorem_ipsum *= (length / lorem_ipsum.size)
+  end
+  lorem_ipsum[0..length]
+end
+
 FactoryGirl.define do
   factory :comment do
-    body 'This is a comment. Useed for testing.'
-    user_id 1
-  end
-  
-  factory :comment2 do
-    body 'This is another comment used for testing. This is is much longer and may or may not actually be usefull.'
-    user_id 1 
+    body lorem(Random.rand(100) + 50)
+    association :user
   end
 end
