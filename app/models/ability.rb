3f4f7046809df06bd1c5fb71cb6d4247327aa4c7
Grayson Wright <wright.grayson@gmail.com>
2013-01-07 18:13:14 -0500

    Heavy cleanup of ability.rb

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -10,23 +10,19 @@ class Ability
 		user ||= User.new
 
 		# Projects
+		can [:create, :save, :upload], Project
+		can [:activate, :destroy], Project, user: user
 		can :read, Project do |p|
-			p.public_can_view? or p.user_id == user.id or p.confirmation_approver?
+			p.public_can_view? or p.user == user or p.confirmation_approver?
 		end
-		can :create, Project
 		can :update, Project do |p|
-			p.user_id == user.id and p.can_edit?
+			p.user == user and p.can_edit?
 		end
-		can :activate, Project, :user_id => user.id
-		
-		can :destroy, Project, :user_id => user.id
-		can :save, Project
-		can :upload, Project
 		
 		# Contributions
 		# Make sure the user isn't a project owner and doesn't have a contribution already
 		can :contribute, Project do |project|
-			!user.id.nil? and project.user_id != user.id and project.contributions.find_by_user_id(user.id).nil? and project.end_date >= Date.today
+			!user.id.nil? and project.user != user and project.contributions.find_by_user_id(user.id).nil? and project.end_date >= Date.today
 		end
 		# If the user is logged in, doesn't own the project,  and has a contribution on this project,
 		# they can edit
@@ -35,73 +31,27 @@ class Ability
 		end
 		
 		# Groups
-		can :read, Group
-		can :create, Group
-		can :new_add, Group
-		can :submit_add, Group
-		can :edit, Group, :admin_user_id => user.id
-		can :update, Group, :admin_user_id => user.id
-		can :admin, Group, :admin_user_id => user.id
-		can :remove_project, Group # had to move check for admin or proeject owner to controller
-		can :add_list, Group, :admin_user_id => user.id
-		can :destroy, Group, :admin_user_id => user.id
+		can [:read, :create, :new_add, :submit_add], Group
+		can :remove_project, Group # had to move check for admin or project owner to controller
+
+    can [:edit, :update, :admin, :add_list, :destroy], Group, admin_user: user
 		
 		#Aprovals
-		can :approve, Approval do |a|
-			Group.find(a.group_id).admin_user_id == user.id
-		end
-		can :reject, Approval do |a|
-			Group.find(a.group_id).admin_user_id == user.id
+		can [:approve, :reject], Approval do |a|
+			a.group.admin_user == user
 		end
 		
-		#Lists (Find a way to make it cleaner/shorter?)
+		#Lists
 		can :read, List
-		can :destroy, List do |l|
+		can [:destroy, :edit, :update, :sort, :add_item], List do |l|
 			if l.listable_type == "Group"
-				l.listable.admin_user_id == user.id
+				l.listable.admin_user == user
 			elsif l.listable_type == "User"
 				l.listable.id == user.id
 			else
 				false
 			end
 		end
-		can :edit, List do |l|
-			if l.listable_type == "Group"
-				l.listable.admin_user_id == user.id
-			elsif l.listable_type == "User"
-				l.listable.id == user.id
-			else
-				false
-			end
-		end
-		can :update, List do |l|
-			if l.listable_type == "Group"
-				l.listable.admin_user_id == user.id
-			elsif l.listable_type == "User"
-				l.listable.id == user.id
-			else
-				false
-			end
-		end
-		can :sort, List do |l|
-			if l.listable_type == "Group"
-				l.listable.admin_user_id == user.id
-			elsif l.listable_type == "User"
-				l.listable.id == user.id
-			else
-				false
-			end
-		end
-		can :add_item, List do |l|
-			if l.listable_type == "Group"
-				l.listable.admin_user_id == user.id
-			elsif l.listable_type == "User"
-				l.listable.id == user.id
-			else
-				false
-			end
-		end
-		
 		
   end
 end
