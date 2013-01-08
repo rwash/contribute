4b33fcc886f23596517de952577823072c433fd1
Grayson Wright <wright.grayson@gmail.com>
2013-01-08 11:34:12 -0500

    Added enumeration for project state

---
+++ app/enums/project_state.rb
@@ -0,0 +1,20 @@
+class ProjectState < ClassyEnum::Base
+end
+
+class ProjectState::Unconfirmed < ProjectState
+end
+
+class ProjectState::Inactive < ProjectState
+end
+
+class ProjectState::Active < ProjectState
+end
+
+class ProjectState::Nonfunded < ProjectState
+end
+
+class ProjectState::Funded < ProjectState
+end
+
+class ProjectState::Canceled < ProjectState
+end
