e6eee56212580a196be49076c07ff752285e54dd
Grayson Wright <wright.grayson@gmail.com>
2013-01-08 16:28:32 -0500

    Migration to correct spelling of 'cancelled' in database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121016161159) do
+ActiveRecord::Schema.define(:version => 20130108211821) do
 
   create_table "amazon_errors", :force => true do |t|
     t.string   "description"
