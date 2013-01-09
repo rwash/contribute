6d5414577f44cf744ffdc13727e7ddf5926ca7ed
Grayson Wright <wright.grayson@gmail.com>
2013-01-09 18:08:46 -0500

    Fixed color of project state span on show page

--- app/enums/project_state.rb
+++ app/enums/project_state.rb
@@ -16,33 +16,46 @@ class ProjectState < ClassyEnum::Base
   # Returns true if users can comment on the project.
   # The project must be active, funded, or non-funded.
   def can_comment?() false end
+
+  # Used for displaying the project throughout the site
+  # Different project states are shown in different colors,
+  # so it's important to be consistent
+  #
+  # TODO: Perhaps there's a better place for this... Decorator, maybe?
+  def color_class() 'important' end
 end
 
 class ProjectState::Unconfirmed < ProjectState
   def can_edit?() true end
+  def color_class() 'warning' end
 end
 
 class ProjectState::Inactive < ProjectState
   def can_edit?() true end
+  def color_class() 'warning' end
 end
 
 class ProjectState::Active < ProjectState
   def public_can_view?() true end
   def can_update?() true end
   def can_comment?() true end
+  def color_class() 'success' end
 end
 
 class ProjectState::Funded < ProjectState
   def public_can_view?() true end
   def can_update?() true end
   def can_comment?() true end
+  def color_class() 'success' end
 end
 
 class ProjectState::Nonfunded < ProjectState
   def public_can_view?() true end
   def can_update?() true end
   def can_comment?() true end
+  def color_class() 'inverse' end
 end
 
 class ProjectState::Cancelled < ProjectState
+  def color_class() 'inverse' end
 end
