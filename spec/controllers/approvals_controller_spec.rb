f94b6c78c8f2566924245e255c0faa19a408538e
Grayson Wright <wright.grayson@gmail.com>
2013-01-16 21:29:56 -0500

    Fixed security hole with approvals controller
    Previously, there was no check to make sure that the
    user performing the approval or rejection was the group
    admin. It slipped through the test case because it didn't
    reload the data from the database.

--- spec/controllers/approvals_controller_spec.rb
+++ spec/controllers/approvals_controller_spec.rb
@@ -5,37 +5,63 @@ describe ApprovalsController do
   include Devise::TestHelpers
 
   let(:user) { Factory :user }
-  before(:each) { sign_in user }
+  before { sign_in user }
 
   context "when signed in as an admin" do
     let(:group) { Factory :group, admin_user: user, open: false }
     let(:approval) { Factory :approval, group: group }
 
-    it "allows approvals", :broken do
-      post 'approve', :group_id => group.id, :id => approval.id
-      approval.reload.approved.should be_true
-      response.should redirect_to(group_admin_path(group))
+    describe 'POST approve' do
+      before do
+        post 'approve', :group_id => group.id, :id => approval.id
+      end
+
+      it "updates approval status" do
+        expect(approval.reload.approved).to be_true
+      end
+
+      it { should redirect_to group_admin_path(group) }
+      it { should_not set_the_flash }
     end
 
-    it "allows rejections", :broken do
-      post 'reject', :group_id => group.id, :id => approval.id, :reason => "I dont know"
-      approval.reload.approved.should be_false
-      response.should redirect_to(group_admin_path(group))
+    describe 'POST reject' do
+      before do
+        post 'reject', :group_id => group.id, :id => approval.id, :reason => "I dont know"
+      end
+
+      it "updates approval status" do
+        expect(approval.reload.approved).to be_false
+      end
+
+      it { should redirect_to group_admin_path(group) }
+      it { should_not set_the_flash }
     end
   end
 
-  context "when not signed in as an admin" do
+  context "when not signed in as admin" do
     let(:group) { Factory :group, open: false }
     let(:approval) { Factory :approval, group: group }
 
-    it "does not allow approvals", :broken do
-      expect {post 'approve', :group_id => group.id, :id => approval.id}.to_not change {approval.approved}
-      response.should redirect_to(group_admin_path(group))
+    describe 'POST approve' do
+      before { post 'approve', :group_id => group.id, :id => approval.id }
+
+      it "does not update approved attribute", :broken do
+        expect(approval.reload.approved).to be_nil
+      end
+
+      it { should redirect_to root_url }
+      it { should set_the_flash }
     end
 
-    it "does not allow rejections", :broken do
-      expect {post 'reject', :group_id => group.id, :id => approval.id, :reason => "I dont know"}.to_not change {approval.approved}
-      response.should redirect_to(group_admin_path(group))
+    describe 'POST reject' do
+      before { post 'reject', :group_id => group.id, :id => approval.id }
+
+      it "does not allow approvals", :broken do
+        expect { post 'reject', :group_id => group.id, :id => approval.id }.to_not change {approval.reload.approved}
+      end
+
+      it { should redirect_to root_url }
+      it { should set_the_flash }
     end
   end
 end
