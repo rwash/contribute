5df9e5b5c6f2b586467c65e773102a63afb9c8f2
Grayson Wright <wright.grayson@gmail.com>
2013-01-16 21:25:01 -0500

    Fixed bug when updating a contribution to the same amount

--- spec/integration/amazon_process_spec.rb
+++ spec/integration/amazon_process_spec.rb
@@ -12,12 +12,6 @@ class AmazonProcessTesting
       @headless.start
     end
 
-    after :all do
-      Project.delete_all
-      Contribution.delete_all
-    end
-
-
     describe 'project' do
       it "create successfully" do
         project = FactoryGirl.build(:project)
@@ -60,10 +54,10 @@ class AmazonProcessTesting
       end
     end
 
-    describe 'contribution' do
-      let(:project) { Factory.create(:project, state: 'active') }
+    describe 'creating contribution' do
+      let(:project) { Factory.create(:project, state: :active) }
 
-      it "with invalid amount should fail" do
+      it "fails with invalid amount" do
         login('thelen56@msu.edu', 'aaaaaa')
 
         #go to project page
@@ -80,7 +74,7 @@ class AmazonProcessTesting
         page.should have_content('prevented this contribution from being saved')
       end
 
-      it "should contribute successfully" do
+      it "succeeds with valid amount" do
         generate_contribution(
           'thelen56@msu.edu', #contribution login
           'aaaaaa',
@@ -89,24 +83,27 @@ class AmazonProcessTesting
           project, #the project to contribute to
           100) #the amount
 
-        last_email.to.should eq(['thelen56@msu.edu'])
-        last_email.subject.should match(project.name)
-        last_email.subject.should match('Your contribution to')
+          last_email.to.should eq(['thelen56@msu.edu'])
+          last_email.subject.should match(project.name)
+          last_email.subject.should match('Your contribution to')
+      end
+    end
+
+    describe 'editing contribution' do
+      let(:project) { Factory :project, state: :active }
+      before(:each) do
+        generate_contribution(
+          'thelen56@msu.edu', #contribution login
+          'aaaaaa',
+          'contribute_testing@hotmail.com', #amazon login
+          'testing',
+          project, #the project to contribute to
+          100) #the amount
       end
 
-      it "with smaller contribution amount should fail"
-      it "with invalid contribution amount should fail"
-      it "should edit contribution successfully"
-
-=begin
-Broken. Perhaps because they were based on a different test.
----
-      it "with smaller contribution amount should fail" do
-        login('thelen56@msu.edu', 'aaaaaa')
+      it "fails with smaller contribution amount" do
         contribution = get_and_assert_contribution(project.id)
-
         visit edit_contribution_path(contribution)
-
         fill_in 'contribution_amount', :with => contribution.amount - 5
         click_button 'Update Contribution'
 
@@ -114,25 +111,29 @@ Broken. Perhaps because they were based on a different test.
         page.should have_content('prevented this contribution from being saved')
       end
 
-      it "with invalid contribution amount should fail" do
-        login('thelen56@msu.edu', 'aaaaaa')
+      it "fails with invalid contribution amount" do
         contribution = get_and_assert_contribution(project.id)
-
         visit edit_contribution_path(contribution)
-
-        fill_in 'contribution_amount', :with => 'you_fail_me'
+        fill_in 'contribution_amount', :with => "invalid amount"
         click_button 'Update Contribution'
 
         page.should have_content('Edit contribution to')
         page.should have_content('prevented this contribution from being saved')
       end
 
-      it "should edit contribution successfully" do
-        login('thelen56@msu.edu', 'aaaaaa')
+      it "fails with same amount" do
         contribution = get_and_assert_contribution(project.id)
-
         visit edit_contribution_path(contribution)
+        fill_in 'contribution_amount', :with => contribution.amount
+        click_button 'Update Contribution'
 
+        page.should have_content('Edit contribution to')
+        page.should have_content('prevented this contribution from being saved')
+      end
+
+      it "succeeds with valid amount" do
+        contribution = get_and_assert_contribution(project.id)
+        visit edit_contribution_path(contribution)
         fill_in 'contribution_amount', :with => contribution.amount + 5
         click_button 'Update Contribution'
 
@@ -150,8 +151,6 @@ Broken. Perhaps because they were based on a different test.
         last_email.subject.should match(project.name)
         last_email.subject.should match('Your edited contribution to')
       end
-=end
     end
   end
 end
-
