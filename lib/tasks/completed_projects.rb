6f872a2caaa295c647dbf5241d1dc8dc2751f3ac
Grayson Wright <wright.grayson@gmail.com>
2013-01-23 14:39:50 -0500

    Fixed indentation of lib files

--- lib/tasks/completed_projects.rb
+++ lib/tasks/completed_projects.rb
@@ -3,56 +3,56 @@
 require 'logger'
 
 class CompletedProjects
-	@@logger = Logger.new('log/cron_log.log')
+  @@logger = Logger.new('log/cron_log.log')
 
-	def self.run
-		@@logger.info "#{Date.today}: Starting completed_projects"
+  def self.run
+    @@logger.info "#{Date.today}: Starting completed_projects"
 
-		projects_to_process = Project.where("end_date < :today AND state = 'active'", { :today => Date.today })
-		@@logger.info "Found #{projects_to_process.size} projects to process"
+    projects_to_process = Project.where("end_date < :today AND state = 'active'", { :today => Date.today })
+    @@logger.info "Found #{projects_to_process.size} projects to process"
 
-		process_projects(projects_to_process)
+    process_projects(projects_to_process)
 
-		@@logger.info "#{Date.today}: All projects have been processed\n"
-	end
+    @@logger.info "#{Date.today}: All projects have been processed\n"
+  end
 
-	def self.run_all
-		@@logger.info "#{Date.today}: Starting all_completed_projects"
+  def self.run_all
+    @@logger.info "#{Date.today}: Starting all_completed_projects"
 
-		all_projects_to_process = Project.where("end_date <= :yesterday AND state = 'active'", { :yesterday => Date.yesterday })
-		@@logger.info "Found #{all_projects_to_process.size} projects to process"
+    all_projects_to_process = Project.where("end_date <= :yesterday AND state = 'active'", { :yesterday => Date.yesterday })
+    @@logger.info "Found #{all_projects_to_process.size} projects to process"
 
-		process_projects(all_projects_to_process)
+    process_projects(all_projects_to_process)
 
-		@@logger.info "#{Date.today}: All projects have been processed\n"
-	end
+    @@logger.info "#{Date.today}: All projects have been processed\n"
+  end
 
-	def self.process_projects(projects)
-		projects.each do |project|
-			if (project.contributions_total < project.funding_goal)
-				@@logger.info "Project with id #{project.id} was not funded"
-				EmailManager.project_not_funded_to_owner(project).deliver
-				project.state = :nonfunded
+  def self.process_projects(projects)
+    projects.each do |project|
+      if (project.contributions_total < project.funding_goal)
+        @@logger.info "Project with id #{project.id} was not funded"
+        EmailManager.project_not_funded_to_owner(project).deliver
+        project.state = :nonfunded
 
-				project.contributions.each do |contribution|
-					@@logger.info "Contribution with id #{contribution.id} is being cancelled"
-					EmailManager.project_not_funded_to_contributor(contribution).deliver
-					contribution.cancel		
-				end
-			else
-				@@logger.info "Project with id #{project.id} was funded"
-				EmailManager.project_funded_to_owner(project).deliver
-				project.state = :funded
+        project.contributions.each do |contribution|
+          @@logger.info "Contribution with id #{contribution.id} is being cancelled"
+          EmailManager.project_not_funded_to_contributor(contribution).deliver
+          contribution.cancel		
+        end
+      else
+        @@logger.info "Project with id #{project.id} was funded"
+        EmailManager.project_funded_to_owner(project).deliver
+        project.state = :funded
 
-				project.contributions.each do |contribution|
-					@@logger.info "Contribution with id #{contribution.id} is being executed"
-					contribution.execute_payment		
-					EmailManager.project_funded_to_contributor(contribution).deliver
-				end
-			end
-		
-			project.active = 0
-			project.save(:validate => false)
-		end
-	end
+        project.contributions.each do |contribution|
+          @@logger.info "Contribution with id #{contribution.id} is being executed"
+          contribution.execute_payment		
+          EmailManager.project_funded_to_contributor(contribution).deliver
+        end
+      end
+
+      project.active = 0
+      project.save(:validate => false)
+    end
+  end
 end
