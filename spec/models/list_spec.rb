fbfde05eb70d7e9d8e445c36f9a7e588e9d10788
Grayson Wright <wright.grayson@gmail.com>
2013-02-02 22:15:12 -0500

    Added test for list kind validation

--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -4,4 +4,23 @@ describe List do
   # Validations
   it { should validate_presence_of :listable_id }
   it { should validate_presence_of :listable_type }
+
+  it { should ensure_inclusion_of(:kind).in_array %w[default
+                                             manual
+                                             created-at-descending
+                                             created-at-ascending
+                                             end-date-descending
+                                             end-date-ascending
+                                             funding-goal-descending
+                                             funding-goal-ascending
+                                             amount-left-to-goal-in-dollars-descending
+                                             amount-left-to-goal-in-dollars-ascending
+                                             amount-left-to-goal-as-percent-descending
+                                             amount-left-to-goal-as-percent-ascending
+                                             amount-donated-in-dollars-descending
+                                             amount-donated-in-dollars-ascending
+                                             amount-donated-as-percent-of-goal-descending
+                                             amount-donated-as-percent-of-goal-ascending
+                                             random-descending
+                                             random-ascending] }
 end
