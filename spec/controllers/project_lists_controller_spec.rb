b3187db1c0d7868e627f4fc9e2bfc35f21665fc3
Grayson Wright <wright.grayson@gmail.com>
2013-02-04 15:30:27 -0500

    Fixed bugs by creating controller for project_list

--- spec/controllers/project_lists_controller_spec.rb
+++ spec/controllers/project_lists_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 require 'controller_helper'
 
-describe ListsController do
+describe ProjectListsController do
   include Devise::TestHelpers
 
   describe 'POST sort' do
@@ -22,13 +22,13 @@ describe ListsController do
         let(:user) { group.admin_user }
 
         it "does not allow group list deletion" do
-          list = FactoryGirl.create(:list, listable_id: group.id, listable_type: "Group")
+          list = FactoryGirl.create(:project_list, listable_id: group.id, listable_type: "Group")
           expect { get :destroy, id: list.id }.to_not change {List.count}
           expect(response).to redirect_to(root_path)
         end
 
         it "does not allow user list deletion" do
-          list = FactoryGirl.create(:list, listable_id: user.id, listable_type: "User")
+          list = FactoryGirl.create(:project_list, listable_id: user.id, listable_type: "User")
           expect { get :destroy, id: list.id }.to_not change {List.count}
           expect(response).to redirect_to(root_path)
         end
@@ -39,13 +39,13 @@ describe ListsController do
       let(:group) { Factory :group, admin_user: user }
 
       it "allows group list destruction" do
-        list = FactoryGirl.create(:list, listable_id: group.id, listable_type: "Group")
+        list = FactoryGirl.create(:project_list, listable_id: group.id, listable_type: "Group")
         expect { get :destroy, id: list.id }.to change {List.count}.by(-1)
         expect(response).to redirect_to(list.listable)
       end
 
       it "allows user list destruction" do
-        list = FactoryGirl.create(:list, listable_id: user.id, listable_type: "User")
+        list = FactoryGirl.create(:project_list, listable_id: user.id, listable_type: "User")
         expect { get :destroy, id: list.id }.to change {List.count}.by(-1)
         expect(response).to redirect_to(list.listable)
       end
@@ -63,13 +63,13 @@ describe ListsController do
         let(:user) { group.admin_user }
 
         it "does not allow group list editing" do
-          list = FactoryGirl.create(:list, listable_id: group.id, listable_type: "Group")
+          list = FactoryGirl.create(:project_list, listable_id: group.id, listable_type: "Group")
           get :edit, id: list.id
           expect(response).to redirect_to(root_path)
         end
 
         it "does not allow user list editing" do
-          list = FactoryGirl.create(:list, listable_id: user.id, listable_type: "User")
+          list = FactoryGirl.create(:project_list, listable_id: user.id, listable_type: "User")
           get :edit, id: list.id
           expect(response).to redirect_to(root_path)
         end
@@ -84,14 +84,14 @@ describe ListsController do
   describe 'GET show' do
     context "when user is not signed in" do
       it "displays users list" do
-        list = FactoryGirl.create(:list, listable_id: Factory(:user).id, listable_type: "User")
+        list = FactoryGirl.create(:project_list, listable_id: Factory(:user).id, listable_type: "User")
         get :show, id: list.id
         expect(response).to be_success
         list.delete
       end
 
       it "displays groups list" do
-        list = FactoryGirl.create(:list, listable_id: Factory(:group).id, listable_type: "Group")
+        list = FactoryGirl.create(:project_list, listable_id: Factory(:group).id, listable_type: "Group")
         get :show, id: list.id
         expect(response).to be_success
         list.delete
