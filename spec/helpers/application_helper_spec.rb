71808d7e7c0ac8c0fb803ae06469cdf450adb558
Grayson Wright <wright.grayson@gmail.com>
2013-02-04 12:37:07 -0500

    Cleaned up application helper spec

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -2,10 +2,6 @@ require 'spec_helper'
 
 describe ApplicationHelper do
   it 'formats date correctly' do
-    date = Date.new(2012, 04, 26)
-
-    formatted = format_date(date)
-
-    expect(formatted).to eq '04/26/2012'
+    expect(format_date Date.new(2012, 04, 26)).to eq '04/26/2012'
   end
 end
