2488d7903984d82637592fa7c1f5b6e05b3d0c60
Grayson Wright <wright.grayson@gmail.com>
2013-02-05 17:04:24 -0500

    Added tests for abilities

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -2,6 +2,73 @@ require 'spec_helper'
 
 describe Group do
 
+  # Abilities
+  describe 'Abilities' do
+    subject { ability }
+    let(:ability) { Ability.new(user) }
+    let(:group) { Factory.build :group }
+
+    context 'when not signed in' do
+      let(:user) { nil }
+
+      it { should be_able_to :read, group }
+      it { should be_able_to :show, group }
+      it { should be_able_to :index, group }
+      it { should_not be_able_to :create, group }
+      it { should_not be_able_to :submit_add, group }
+      it { should_not be_able_to :new_add, group }
+      it { should_not be_able_to :edit, group }
+      it { should_not be_able_to :update, group }
+      it { should_not be_able_to :admin, group }
+      it { should_not be_able_to :destroy, group }
+    end
+
+    context 'when signed in' do
+      let(:user) { Factory :user }
+
+      it { should be_able_to :read, group }
+      it { should be_able_to :show, group }
+      it { should be_able_to :index, group }
+      it { should be_able_to :create, group }
+      it { should be_able_to :submit_add, group }
+      it { should be_able_to :new_add, group }
+      it { should_not be_able_to :edit, group }
+      it { should_not be_able_to :update, group }
+      it { should_not be_able_to :admin, group }
+      it { should_not be_able_to :destroy, group }
+    end
+
+    context 'when user owns group' do
+      let(:user) { group.admin_user }
+
+      it { should be_able_to :read, group }
+      it { should be_able_to :show, group }
+      it { should be_able_to :index, group }
+      it { should be_able_to :create, group }
+      it { should be_able_to :submit_add, group }
+      it { should be_able_to :new_add, group }
+      it { should be_able_to :edit, group }
+      it { should be_able_to :update, group }
+      it { should be_able_to :admin, group }
+      it { should be_able_to :destroy, group }
+    end
+
+    context 'when signed in as admin' do
+      let(:user) { Factory :user, admin: true }
+
+      it { should be_able_to :read, group }
+      it { should be_able_to :show, group }
+      it { should be_able_to :index, group }
+      it { should be_able_to :create, group }
+      it { should be_able_to :submit_add, group }
+      it { should be_able_to :new_add, group }
+      it { should be_able_to :edit, group }
+      it { should be_able_to :update, group }
+      it { should be_able_to :admin, group }
+      it { should be_able_to :destroy, group }
+    end
+  end
+
   it { should validate_presence_of :name }
   it { should validate_uniqueness_of(:name).case_insensitive }
   it { should validate_presence_of :admin_user }
--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe List do
+
   # Validations
   it { should validate_presence_of :listable_id }
   it { should validate_presence_of :listable_type }
