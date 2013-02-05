5a501bc096e4fcd58aefbf2f90829cbcd8dd84ce
Grayson Wright <wright.grayson@gmail.com>
2013-02-04 22:52:36 -0500

    Began stubbing CanCan abilities in controller specs

--- spec/controllers/listings_controller_spec.rb
+++ spec/controllers/listings_controller_spec.rb
@@ -4,11 +4,37 @@ require 'controller_helper'
 describe ListingsController do
   include Devise::TestHelpers
 
-  describe 'DELETE destroy' do
-    # TODO create Listing factory
-    let(:listing) { Listing.create(project_id: 2) }
-    before { delete :destroy, id: listing.id }
+  # For stubbing abilities
+  # See https://github.com/ryanb/cancan/wiki/Testing-Abilities
+  before do
+    @ability = Object.new
+    @ability.extend(CanCan::Ability)
+    controller.stub!(:current_ability).and_return(@ability)
+  end
 
-    it { should redirect_to :root }
+  describe 'DELETE destroy' do
+    context 'without permission' do
+      before { @ability.stub!(:can?).and_return(false) }
+      before { delete :destroy, id: Factory(:project_listing) }
+
+      it 'does not destroy the record' do
+        listing = Factory(:project_listing)
+        expect { delete :destroy, id: listing }.to_not change { ProjectListing.count }
+      end
+      it { should redirect_to :root }
+      it { should set_the_flash }
+    end
+
+    context 'with permission' do
+      before { @ability.stub!(:can?).and_return(true) }
+      before { delete :destroy, id: Factory(:project_listing) }
+
+      it 'destroys the record' do
+        listing = Factory(:project_listing)
+        expect { delete :destroy, id: listing }.to change { ProjectListing.count }.by(-1)
+      end
+      it { should redirect_to :root }
+      it { should_not set_the_flash }
+    end
   end
 end
