6a4effae38a4e4148c5559f5a4fc25f2c7a6787b
Grayson Wright <wright.grayson@gmail.com>
2013-02-04 23:12:32 -0500

    Stubbed CanCan abilities in VideosController spec

--- spec/controllers/videos_controller_spec.rb
+++ spec/controllers/videos_controller_spec.rb
@@ -4,26 +4,41 @@ require 'controller_helper'
 describe VideosController do
   include Devise::TestHelpers
 
+  # For stubbing CanCan Abilities
+  # See https://github.com/ryanb/cancan/wiki/Testing-Abilities
+  before do
+    @ability = Object.new
+    @ability.extend(CanCan::Ability)
+    controller.stub!(:current_ability).and_return(@ability)
+  end
+
+  let(:user) { Factory :user }
+
   describe 'DELETE destroy' do
-    context 'when video owner is signed in' do
-      let(:video) { Factory :video }
-      before { sign_in video.project.user }
-      before { delete :destroy, id: video.id }
+    context 'while signed in' do
+      before { sign_in user }
 
-      it { should set_the_flash.to(/Successfully Deleted/) }
-      it { should redirect_to project_path(video.project) }
+      context 'with permission' do
+        before { @ability.stub!(:can?).with(:destroy, video).and_return(true) }
+        let(:video) { Factory :video }
+
+        before { delete :destroy, id: video.id }
+
+        it { should set_the_flash.to(/Successfully Deleted/) }
+        it { should redirect_to project_path(video.project) }
+      end
+
+      context 'when user does not own video' do
+        before { @ability.stub!(:can?).with(:destroy, video).and_return(false) }
+        let(:video) { Factory :video }
+        before { delete :destroy, id: video.id }
+
+        it { should set_the_flash.to(/not authorized/) }
+        it { should redirect_to :root }
+      end
     end
 
-    context 'when user does not own video' do
-      let(:video) { Factory :video }
-      before { sign_in Factory :user }
-      before { delete :destroy, id: video.id }
-
-      it { should set_the_flash.to(/can not delete/) }
-      it { should redirect_to :root }
-    end
-
-    context 'when video owner is not signed in' do
+    context 'while not signed in' do
       let(:video) { Factory :video }
       before { delete :destroy, id: video.id }
 
