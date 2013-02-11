309e2481c0ea06752e1ef701abd0630f90d936ef
Grayson Wright <wright.grayson@gmail.com>
2013-02-11 18:12:27 -0500

    Added project state 'blocked'

--- spec/enums/project_state_spec.rb
+++ spec/enums/project_state_spec.rb
@@ -4,16 +4,11 @@ describe ProjectState do
   let(:project) { create :project, state: project_state }
 
   context "is unconfirmed" do
-    let(:project_state) { 'unconfirmed' }
+    let(:project_state) { :unconfirmed }
 
     it 'responds appropriately' do
       expect(project.state).to eq 'unconfirmed'
       expect(project.state.unconfirmed?).to be_true
-      expect(project.state.inactive?).to be_false
-      expect(project.state.active?).to be_false
-      expect(project.state.nonfunded?).to be_false
-      expect(project.state.funded?).to be_false
-      expect(project.state.cancelled?).to be_false
     end
 
     it 'can be edited' do
@@ -34,17 +29,12 @@ describe ProjectState do
   end
 
   context "is inactive" do
-    let(:project_state) { 'inactive' }
+    let(:project_state) { :inactive }
 
     it 'responds appropriately' do
       project.state = :inactive
       expect(project.state).to eq 'inactive'
-      expect(project.state.unconfirmed?).to be_false
       expect(project.state.inactive?).to be_true
-      expect(project.state.active?).to be_false
-      expect(project.state.nonfunded?).to be_false
-      expect(project.state.funded?).to be_false
-      expect(project.state.cancelled?).to be_false
     end
 
     it "can be edited" do
@@ -65,17 +55,12 @@ describe ProjectState do
   end
 
   context "is active" do
-    let(:project_state) { 'active' }
+    let(:project_state) { :active }
 
     it 'responds appropriately' do
       project.state = :active
       expect(project.state).to eq 'active'
-      expect(project.state.unconfirmed?).to be_false
-      expect(project.state.inactive?).to be_false
       expect(project.state.active?).to be_true
-      expect(project.state.nonfunded?).to be_false
-      expect(project.state.funded?).to be_false
-      expect(project.state.cancelled?).to be_false
     end
 
     it "can't be edited" do
@@ -96,17 +81,12 @@ describe ProjectState do
   end
 
   context "is nonfunded" do
-    let(:project_state) { 'nonfunded' }
+    let(:project_state) { :nonfunded }
 
     it 'responds appropriately' do
       project.state = :nonfunded
       expect(project.state).to eq 'nonfunded'
-      expect(project.state.unconfirmed?).to be_false
-      expect(project.state.inactive?).to be_false
-      expect(project.state.active?).to be_false
       expect(project.state.nonfunded?).to be_true
-      expect(project.state.funded?).to be_false
-      expect(project.state.cancelled?).to be_false
     end
 
     it "can't be edited" do
@@ -127,17 +107,12 @@ describe ProjectState do
   end
 
   context "is funded" do
-    let(:project_state) { 'funded' }
+    let(:project_state) { :funded }
 
     it 'responds appropriately' do
       project.state = :funded
       expect(project.state).to eq 'funded'
-      expect(project.state.unconfirmed?).to be_false
-      expect(project.state.inactive?).to be_false
-      expect(project.state.active?).to be_false
-      expect(project.state.nonfunded?).to be_false
       expect(project.state.funded?).to be_true
-      expect(project.state.cancelled?).to be_false
     end
 
     it "can't be edited" do
@@ -158,21 +133,42 @@ describe ProjectState do
   end
 
   context "is cancelled" do
-    let(:project_state) { 'cancelled' }
+    let(:project_state) { :cancelled }
 
     it 'responds appropriately' do
       project.state = :cancelled
       expect(project.state).to eq 'cancelled'
-      expect(project.state.unconfirmed?).to be_false
-      expect(project.state.inactive?).to be_false
-      expect(project.state.active?).to be_false
-      expect(project.state.nonfunded?).to be_false
-      expect(project.state.funded?).to be_false
       expect(project.state.cancelled?).to be_true
     end
 
     it "can't be edited" do
       expect(project.can_edit?).to be_false
+    end
+
+    it "can't be viewed by the public" do
+      expect(project.public_can_view?).to be_false
+    end
+
+    it "can't be updated" do
+      expect(project.can_update?).to be_false
+    end
+
+    it "can't be commented on" do
+      expect(project.can_comment?).to be_false
+    end
+  end
+
+  context 'is blocked' do
+    let(:project_state) { :blocked }
+
+    it 'responds appropriately' do
+      project.state = :cancelled
+      expect(project.state).to eq 'cancelled'
+      expect(project.state.cancelled?).to be_true
+    end
+
+    it "can be edited" do
+      expect(project.can_edit?).to be_true
     end
 
     it "can't be viewed by the public" do
