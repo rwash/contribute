6dc22602e6d2f706fe5cd6a6133a6f72d7d112e6
Grayson Wright <wright.grayson@gmail.com>
2013-06-07 17:56:52 +0800

    Add feature specs for creating new comments

--- spec/features/comments_spec.rb
+++ spec/features/comments_spec.rb
@@ -2,8 +2,39 @@ require 'spec_helper'
 require 'integration_helper'
 
 feature 'comments', :js do
-  # TODO feature for creating root-level comments
-  # TODO feature for creating reply comments
+  scenario 'User creates a root-level comment' do
+    project = create :active_project
+    comment = build :comment
+    login_as create :user
+
+    visit project_path project
+    click_on "Comments"
+    fill_in :comment_body, with: comment.body
+    click_on "Create Comment"
+
+    click_on "Comments"
+    page.should have_content comment.body
+  end
+
+  scenario 'User creates a nested comment' do
+    project = create :active_project
+    root_comment = create :comment, commentable: project
+    comment = build :comment
+    login_as create :user
+
+    visit project_path project
+    click_on "Comments"
+    within '.projectComments' do
+      click_on "reply"
+      within 'form#new_comment' do
+        fill_in :comment_body, with: comment.body
+        click_on 'Create Comment'
+      end
+    end
+
+    click_on "Comments"
+    page.should have_content comment.body
+  end
 
   scenario 'User deletes a root-level comment' do
     project = create :active_project
