8e08b899df72e4f1994dde060e72629073459ae5
Grayson Wright <wright.grayson@gmail.com>
2013-06-07 17:31:59 +0800

    Add more feature specs for deleting comments

--- spec/features/comments_spec.rb
+++ spec/features/comments_spec.rb
@@ -20,4 +20,38 @@ feature 'comments', :js do
     page.should_not have_content comment.body
     page.should_not have_content "[comment deleted]"
   end
+
+  scenario 'User destroys a root-level comment with replies' do
+    project = create :active_project
+    root_comment = create(:comment, commentable: project)
+    nested_comment = create(:comment, commentable: project, parent: root_comment, body: "Nested Comment")
+    login_as root_comment.user
+
+    visit project_path root_comment.commentable
+    click_on 'Comments'
+    page.should have_content root_comment.body
+
+    click_on 'delete'
+    click_on 'Comments'
+    page.should have_content 'Add a comment'
+    page.should_not have_content root_comment.body
+    page.should have_content "[comment deleted]"
+  end
+
+  scenario 'User destroys a nested comment' do
+    project = create :active_project
+    root_comment = create(:comment, commentable: project)
+    nested_comment = create(:comment, commentable: project, parent: root_comment, body: "Nested Comment")
+    login_as nested_comment.user
+
+    visit project_path nested_comment.commentable
+    click_on 'Comments'
+    page.should have_content nested_comment.body
+
+    click_on 'delete'
+    click_on 'Comments'
+    page.should have_content 'Add a comment'
+    page.should_not have_content nested_comment.body
+    page.should_not have_content "[comment deleted]"
+  end
 end
