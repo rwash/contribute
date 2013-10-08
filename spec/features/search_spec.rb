6e55cd3551cd5f08719b2ef8dd3351b31086c016
Grayson Wright <wright.grayson@gmail.com>
2013-10-08 17:38:22 -0400

    Add scenario for a positive match

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 feature 'Search' do
   context 'with no projects' do
     it 'displays an appropriate message' do
-      search_for 'Unicorn'
+      search_for search_term
       page.should have_content 'no projects were found'
     end
   end
@@ -13,10 +13,24 @@ feature 'Search' do
     page.should_not have_content 'no projects were found'
   end
 
+  context 'with a single matching active project' do
+    let!(:matching_project) { create :active_project, name: search_term }
+
+    it 'displays information for the matching project' do
+      search_for search_term
+      page.should have_content matching_project.name
+      page.should have_content matching_project.short_description
+    end
+  end
+
   private
   def search_for query
     visit projects_path
     fill_in 'search', with: query
     click_button 'Search'
+  end
+
+  def search_term
+    'unicorn'
   end
 end
