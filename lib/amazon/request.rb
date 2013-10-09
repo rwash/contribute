beedc017ffaf396ec261ebeb4091a5ab2f397aaa
Grayson Wright <wright.grayson@gmail.com>
2013-10-09 12:03:45 -0400

    Simplify Amazon::Request
    Extract methods, remove unneccesary arguments,
    and rename some attributes

--- lib/amazon/request.rb
+++ lib/amazon/request.rb
@@ -4,6 +4,8 @@ module Amazon
     end
 
     protected
+    attr_reader :params
+
     def http_method
       "GET"
     end
@@ -36,41 +38,40 @@ module Amazon
     end
 
     def signature
-      @_signature ||= begin
-                        uri = URI.parse(service_end_point)
-                        sign_parameters({
-                          parameters: @params,
-                          aws_secret_key: secret_key,
-                          host: uri.host,
-                          verb: http_method,
-                          uri: uri.path,
-                        })
-                      end
+      @_signature ||= sign_parameters
     end
 
-    def sign_parameters(args)
-      signature_version = args[:parameters][Amazon::FPS::SignatureUtils::SIGNATURE_VERSION_KEYNAME]
+    def uri
+      URI.parse(service_end_point)
+    end
+
+    def uri_host
+      uri.host
+    end
+
+    def uri_path
+      uri.path
+    end
+
+    def sign_parameters
+      signature_version = params[Amazon::FPS::SignatureUtils::SIGNATURE_VERSION_KEYNAME]
       string_to_sign = "";
       algorithm = 'sha1';
       if (signature_version == '1') then
-        string_to_sign = calculate_string_to_sign_v1(args)
+        string_to_sign = calculate_string_to_sign_v1
       elsif (signature_version == '2') then
-        algorithm = get_algorithm(args[:parameters][Amazon::FPS::SignatureUtils::SIGNATURE_METHOD_KEYNAME])
-        string_to_sign = calculate_string_to_sign_v2(args)
+        algorithm = get_algorithm(params[Amazon::FPS::SignatureUtils::SIGNATURE_METHOD_KEYNAME])
+        string_to_sign = calculate_string_to_sign_v2
       else
         raise "Invalid Signature Version specified"
       end
-      return compute_signature(string_to_sign, args[:aws_secret_key], algorithm)
+      return compute_signature(string_to_sign, algorithm)
     end
 
-    def calculate_string_to_sign_v1(args)
-      parameters = args[:parameters]
-
+    def calculate_string_to_sign_v1
       # exclude any existing Signature parameter from the canonical string
-      sorted = (parameters.reject { |k, v| k == Amazon::FPS::SignatureUtils::SIGNATURE_KEYNAME }).sort { |a,b| a[0].downcase <=> b[0].downcase }
-
       canonical = ''
-      sorted.each do |v|
+      signable_params.each do |v|
         canonical << v[0]
         canonical << v[1] unless(v[1].nil?)
       end
@@ -78,26 +79,27 @@ module Amazon
       return canonical
     end
 
-    def calculate_string_to_sign_v2(args)
-      parameters = args[:parameters]
-
-      uri = args[:uri]
+    def calculate_string_to_sign_v2
+      uri = uri_path
       uri = "/" if uri.nil? or uri.empty?
       uri = urlencode(uri).gsub("%2F", "/")
 
-      verb = args[:verb]
-      host = args[:host].downcase
+      verb = http_method
+      host = uri_host.downcase
 
       canonical_header = "#{verb}\n#{host}\n#{uri}\n"
 
-      acceptable_params = parameters.reject { |k, v| k == Amazon::FPS::SignatureUtils::SIGNATURE_KEYNAME }
-      sorted_params = acceptable_params.sort
-
-      canonical_params = sorted_params.map do |assignment|
+      canonical_params = signable_params.map do |assignment|
         assignment.map {|element| urlencode(element)}.join '='
       end.join '&'
 
       canonical_header + canonical_params
+    end
+
+    def signable_params
+      params.reject do |k, v|
+        k == Amazon::FPS::SignatureUtils::SIGNATURE_KEYNAME
+      end.sort
     end
 
     def get_algorithm(signature_method)
@@ -108,9 +110,9 @@ module Amazon
       end
     end
 
-    def compute_signature(canonical, aws_secret_key, algorithm = 'sha1')
+    def compute_signature(canonical, algorithm = 'sha1')
       digest = OpenSSL::Digest::Digest.new(algorithm)
-      return Base64.encode64(OpenSSL::HMAC.digest(digest, aws_secret_key, canonical)).chomp
+      return Base64.encode64(OpenSSL::HMAC.digest(digest, secret_key, canonical)).chomp
     end
 
     def urlencode(string)
