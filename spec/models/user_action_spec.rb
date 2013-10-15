4cf24481c2079fc1889b0448424929ef98f8feb0
Grayson Wright <wright.grayson@gmail.com>
2013-10-15 14:55:52 -0400

    Create association between UserAction and other objects

--- spec/models/user_action_spec.rb
+++ spec/models/user_action_spec.rb
@@ -1,5 +1,20 @@
 require 'spec_helper'
 
 describe UserAction do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'can assign projects as an object' do
+    action.object = project
+    action.save.should be_true
+    action.object.should eq project
+  end
+
+  it 'can assign contributions as an object' do
+    action.object = contribution
+    action.save.should be_true
+    action.object.should eq contribution
+  end
+
+  private
+  let(:action) { UserAction.new }
+  let(:project) { create :project }
+  let(:contribution) { create :contribution }
 end
