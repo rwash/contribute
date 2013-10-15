41713b6d2d3043a2374907e5d1e308ef8347df4e
Grayson Wright <wright.grayson@gmail.com>
2013-10-15 18:52:10 -0400

    Log group creation & deletion

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -14,6 +14,7 @@ class GroupsController < InheritedResources::Base
     @group.owner = current_user
 
     if @group.save!
+      log_user_action :create, @group.name
       redirect_to @group, notice: "Successfully created group."
     else
       redirect_to new_group_path, error: "Failed to save group. Please try again."
@@ -26,6 +27,7 @@ class GroupsController < InheritedResources::Base
     for proj in current_user.projects
       @projects << proj if !@group.projects.include?(proj) && proj.state != 'cancelled'
     end
+    log_user_action :new_add
   end
 
   def submit_add
@@ -44,11 +46,13 @@ class GroupsController < InheritedResources::Base
       project.update_project_video unless video.nil?
 
       flash[:notice] = "Your project has been added to the group."
+      log_user_action :submit_add, params
     elsif project.approvals.where(group_id: group.id, status: :pending).any?
       flash[:error] = "You have already submitted this project. Please wait for the admin to approve or reject your request."
     elsif group.owner == current_user
       group.projects << project
       flash[:notice] = "Your project has been added."
+      log_user_action :submit_add, params
     else
       approval = Approval.create(group: group, project: project)
       flash[:notice] = "Your project has been submitted to the group admin for approval."
@@ -64,12 +68,13 @@ class GroupsController < InheritedResources::Base
   end
 
   def remove_project
-    group = Group.find(params[:id])
+    @group = Group.find(params[:id])
     project = Project.find(params[:project_id].gsub(/-/, ' '))
-    if group.owner == current_user or project.owner == current_user
-      group.projects.delete(project)
+    if current_user_can_edit_project? project
+      @group.projects.delete(project)
       project.update_project_video
-      flash[:notice] = "#{project.name} removed from group #{group.name}."
+      log_user_action :remove_project, params
+      flash[:notice] = "#{project.name} removed from group #{@group.name}."
     else
       flash[:error] = "You do not have permission to remove this project."
     end
@@ -77,13 +82,26 @@ class GroupsController < InheritedResources::Base
     begin
       redirect_to :back
     rescue
-      redirect_to :group
+      redirect_to @group
     end
   end
 
   def destroy
-    group = Group.find(params[:id])
-    flash[:error] = "Failed to delete group. Please try again." unless group.destroy
+    @group = Group.find(params[:id])
+    if @group.destroy
+      log_user_action :destroy
+    else
+      flash[:error] = "Failed to delete group. Please try again."
+    end
     redirect_to groups_path
+  end
+
+  private
+  def log_user_action event, message=nil
+    UserAction.create user: current_user, subject: @group, event: event, message: message
+  end
+
+  def current_user_can_edit_project? project
+    [@group.owner, project.owner].include? current_user
   end
 end
