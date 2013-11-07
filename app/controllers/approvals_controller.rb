dc55bdd0b872ea07f778a7e5e547d6a76215cac9
Grayson Wright <wright.grayson@gmail.com>
2013-11-07 16:44:19 -0500

    Clean up approvals index

--- app/controllers/approvals_controller.rb
+++ app/controllers/approvals_controller.rb
@@ -1,6 +1,6 @@
 class ApprovalsController < InheritedResources::Base	
 
-  load_and_authorize_resource
+  load_and_authorize_resource except: :index
   before_filter :authenticate_user!, only: [:index, :new, :create]
 
   def index
