34627b618d1b6998be1b0a6b1f62104796454b71
Grayson Wright <wright.grayson@gmail.com>
2013-12-10 16:02:30 -0500

    Refactor BrainTree controller

--- app/controllers/braintree_payment_accounts_controller.rb
+++ app/controllers/braintree_payment_accounts_controller.rb
@@ -2,52 +2,27 @@ class BraintreePaymentAccountsController < ApplicationController
   skip_authorization_check
 
   def new
-    unless current_user == project.owner
-      unauthorized
-    end
+    authorize project.owner
   end
 
   def create
-    unless current_user == project.owner
-      return unauthorized
+    authorize project.owner do
+      braintree_params = params[:braintree_payment_account]
+      result = register_braintree_merchant braintree_params
+      begin
+        BraintreePaymentAccount.create token: result.merchant_account.id, project: project
+        project.state = :inactive
+        project.save
+        redirect_to project
+      rescue
+        flash[:alert] = "There was a problem with the information you entered. Please try again."
+        render :new
+      end
     end
-    application = params[:braintree_payment_account]
-    result = Braintree::MerchantAccount.create(
-      :applicant_details => {
-        :first_name => application[:first_name],
-        :last_name => application[:last_name],
-        :email => application[:email],
-        :address => {
-          :street_address => application[:street_address],
-          :postal_code => application[:postal_code],
-          :locality => application[:locality],
-          :region => application[:region],
-        },
-        :date_of_birth => application[:date_of_birth],
-        :routing_number => application[:routing_number],
-        :account_number => application[:account_number],
-      },
-      :tos_accepted => application[:tos_accepted],
-      :master_merchant_account_id => merchant_account_id,
-    )
-    begin
-      BraintreePaymentAccount.create token: result.merchant_account.id, project: project
-      project.state = :inactive
-      project.save
-      redirect_to project
-    rescue
-      flash[:alert] = "There was a problem with the information you entered. Please try again."
-      render :new
-    end
-  end
-
-  def save
   end
 
   def destroy
-    unless current_user == project.owner
-      return unauthorized
-    end
+    authorize project.owner
   end
 
   private
@@ -55,11 +30,36 @@ class BraintreePaymentAccountsController < ApplicationController
     @_project ||= Project.find_by_slug! params[:project_id]
   end
 
-  def unauthorized
-    redirect_to :root, alert: "You are not authorized to access that page."
+  def authorize user
+    if current_user == user
+      yield if block_given?
+    else
+      redirect_to :root, alert: "You are not authorized to access that page."
+    end
   end
 
   def merchant_account_id
     Contribute::Application.config.braintree_merchant_account_id
+  end
+
+  def register_braintree_merchant params
+    Braintree::MerchantAccount.create(
+      :applicant_details => {
+        :first_name => params[:first_name],
+        :last_name => params[:last_name],
+        :email => params[:email],
+        :address => {
+          :street_address => params[:street_address],
+          :postal_code => params[:postal_code],
+          :locality => params[:locality],
+          :region => params[:region],
+        },
+        :date_of_birth => params[:date_of_birth],
+        :routing_number => params[:routing_number],
+        :account_number => params[:account_number],
+      },
+      :tos_accepted => params[:tos_accepted],
+      :master_merchant_account_id => merchant_account_id,
+    )
   end
 end
