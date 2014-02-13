e21394beeb878212936e6bf14e9cc5d9af8380d9
Grayson Wright <wright.grayson@gmail.com>
2014-02-13 14:12:24 -0500

    WIP remove nested comments

--- spec/features/comments_spec.rb
+++ spec/features/comments_spec.rb
@@ -16,26 +16,6 @@ feature 'comments', :js do
     page.should have_content comment.body
   end
 
-  scenario 'User creates a nested comment' do
-    project = create :active_project
-    root_comment = create :comment, commentable: project
-    comment = build :comment
-    login_as create :user
-
-    visit project_path project
-    click_on "Comments"
-    within '.projectComments' do
-      click_on "reply"
-      within 'form#new_comment' do
-        fill_in :comment_body, with: comment.body
-        click_on 'Post Comment'
-      end
-    end
-
-    click_on "Comments"
-    page.should have_content comment.body
-  end
-
   scenario 'User deletes a root-level comment' do
     project = create :active_project
     comment = create(:comment, commentable: project)
