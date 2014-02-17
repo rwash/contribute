da8edbbb4daf8f293c5f56e4de19cc985c624e47
Grayson Wright <wright.grayson@gmail.com>
2014-02-17 13:29:54 -0500

    Get rid of image upload option in ckeditor bar
    CKEditor removed the option of uploading inline images with their most
    recent version. We can add image uploading in later through a custom
    image uploader, but until then we don't want to display their broken
    image form to our users.

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -3,7 +3,7 @@ CKEDITOR.editorConfig = function( config )
   config.toolbar_Custom = [
     { name: 'tools',       items : [ 'Maximize' ] },
     { name: 'clipboard',   items : [ 'Undo','Redo' ] },
-    { name: 'insert',      items : [ 'Image','Table','HorizontalRule','SpecialChar' ] },
+    { name: 'insert',      items : [ 'Table','HorizontalRule','SpecialChar' ] },
     { name: 'paragraph',   items : [ 'NumberedList','BulletedList','-','Outdent','Indent','-'] },
     { name: 'alignment',   items : [ 'JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock','-' ] },
     { name: 'links',       items : [ 'Link','Unlink' ] },
